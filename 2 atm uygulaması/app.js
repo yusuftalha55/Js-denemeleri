0989c670a25fff8b1a2b211acdce739056958745
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-10-09 20:05:18 +0300

    Çalışan hali

--- 2 atm uygulaması/app.js
+++ 2 atm uygulaması/app.js
@@ -1,12 +1,27 @@
-let bakiye= 15000;
-document.getElementById("yazdır").innerHTML=" Bakiye Görüntüleme: " + bakiye;
+let bakiye = 15000;
 
-function getMoney(){
+document.getElementById("yazdır").innerHTML = " Bakiye Görüntüleme: " + bakiye;
+// let paraCek = parseFloat(document.getElementById("money-out").value);
+// let paraYatır = parseFloat(document.getElementById("money-in").value);
 
 
+function getMoney() {
+
+    let paraCek = parseFloat(document.getElementById("money-out").value);
+
+    if (bakiye >= paraCek) {
+        bakiye -= paraCek;
+    }
+    else {
+        alert("Lütfen paranızdan fazla çekim yapmaya çalışmayın!!!")
+    }
+    document.getElementById("yazdır").innerHTML = " Bakiye Görüntüleme: " + bakiye;
 }
 
-function setMoney(){
+function setMoney() {
+    let paraYatır = parseFloat(document.getElementById("money-in").value);
+    bakiye = bakiye + paraYatır;
+    document.getElementById("yazdır").innerHTML = " Bakiye Görüntüleme: " + bakiye;
 
 }
 
