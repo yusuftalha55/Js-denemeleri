f7f4254044a6ccc2521db2f0c9769aa2a44b93b4
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-10-16 14:19:15 +0300

    image slider 4 bitti

---
+++ 4 image slider/app.js
@@ -0,0 +1,18 @@
+const resimler =["img/img1.jpg" , "img/img2.jpg" , "img/img3.jpg" , "img/img4.jpg" ];
+let gosterilenResimIndex = 0;
+const sliderImage = document.getElementById("sliderImage");
+
+
+function nextClick(){
+    if(gosterilenResimIndex < resimler.length-1){
+        gosterilenResimIndex++;
+    }
+
+    else{
+        gosterilenResimIndex=0;
+    }
+
+    sliderImage.src = resimler[gosterilenResimIndex];
+
+
+}
