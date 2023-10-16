e57081e72d17b7ad6116628e3313d9d6af515a08
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-10-16 14:48:36 +0300

    image slider 5 bitti

---
+++ 5 image slider/app.js
@@ -0,0 +1,14 @@
+const resimler =["img/img1.jpg" , "img/img2.jpg" , "img/img3.jpg" , "img/img4.jpg"];
+let resimIndex =0;
+const sliderImage = document.getElementById("sliderImage");
+
+function nextClick(){
+    if(resimIndex < resimler.length -1){
+        resimIndex++;
+    }
+
+    else{
+        resimIndex=0;
+    }
+    sliderImage.src =resimler[resimIndex];
+};
