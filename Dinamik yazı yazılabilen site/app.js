36ac390e139a6bda1163d18a7f55b89e5fd56100
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-10-17 17:25:49 +0300

    Local storage ve silme özelliği ekledim

--- Dinamik yazı yazılabilen site/app.js
+++ Dinamik yazı yazılabilen site/app.js
@@ -1,18 +1,46 @@
-function card1Click(){
+function card1Click() {
     let input1 = document.getElementById("input1").value;
     document.getElementById("card1").innerHTML = input1;
+    localStorage.setItem("input1Verisi", input1);
+}
 
-};
-
-function card2Click(){
+function card2Click() {
     let input2 = document.getElementById("input2").value;
     document.getElementById("card2").innerHTML = input2;
-    
-};
+    localStorage.setItem("input2Verisi", input2);
+}
 
-function card3Click(){
+function card3Click() {
     let input3 = document.getElementById("input3").value;
-    document.getElementById("card3").innerHTML = input3;
-    
-    
-};
+    localStorage.setItem("input3Verisi", input3);
+    let sabit = localStorage.getItem("input3Verisi");
+    document.getElementById("card3").innerHTML = sabit;
+}
+
+function card1Click1() {
+    document.getElementById("card1").innerHTML = "";
+    localStorage.removeItem("input1Verisi"); 
+}
+
+function card2Click2() {
+    document.getElementById("card2").innerHTML = "";
+    localStorage.removeItem("input2Verisi");
+}
+
+function card3Click3() {
+    document.getElementById("card3").innerHTML = "";
+    localStorage.removeItem("input3Verisi");
+}
+
+
+
+
+window.onload = function() {
+    let storedInput1 = localStorage.getItem("input1Verisi");
+    let storedInput2 = localStorage.getItem("input2Verisi");
+    let storedInput3 = localStorage.getItem("input3Verisi");
+
+    document.getElementById("card1").innerHTML = storedInput1 || "";
+    document.getElementById("card2").innerHTML = storedInput2 || "";
+    document.getElementById("card3").innerHTML = storedInput3 || "";
+}
