c55f50ad2ee7979c168f880f7179ab33ab285676
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-10-17 16:44:38 +0300

    Site bitti

---
+++ Dinamik yazı yazılabilen site/app.js
@@ -0,0 +1,18 @@
+function card1Click(){
+    let input1 = document.getElementById("input1").value;
+    document.getElementById("card1").innerHTML = input1;
+
+};
+
+function card2Click(){
+    let input2 = document.getElementById("input2").value;
+    document.getElementById("card2").innerHTML = input2;
+    
+};
+
+function card3Click(){
+    let input3 = document.getElementById("input3").value;
+    document.getElementById("card3").innerHTML = input3;
+    
+    
+};
