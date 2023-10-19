54cd120c5e91d626384e2f81b4855b1f90ebdda8
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-10-19 18:10:26 +0300

    our rewievs ana hatları tamam birazda js

---
+++ our reviews/app.js
@@ -0,0 +1,58 @@
+let araba1 = {
+    img1: "img/img1.jpg",
+    not1: `Elektrikli Arabaların Yükselişi:
+        Günümüzde çevre dostu teknolojilere olan talep arttıkça, 
+        elektrikli arabalar piyasadaki önemli bir yer ediniyor.
+        Bu araçlar, fosil yakıt kullanımını azaltarak çevreye daha az zarar veriyor.
+        Ayrıca, elektrikli arabaların tasarımı ve teknolojisi giderek daha çekici ve gelişmiş hale geliyor.`
+};
+
+let araba2 = {
+    img2: "img/img2.jpg",
+    not2: `Elektrikli Arabaların Yükselişi:
+        Günümüzde çevre dostu teknolojilere olan talep arttıkça, 
+        elektrikli arabalar piyasadaki önemli bir yer ediniyor.
+        Bu araçlar, fosil yakıt kullanımını azaltarak çevreye daha az zarar veriyor.
+        Ayrıca, elektrikli arabaların tasarımı ve teknolojisi giderek daha çekici ve gelişmiş hale geliyor.`
+};
+
+let araba3 = {
+    img3: "img/img3.jpg",
+    not3: `Elektrikli Arabaların Yükselişi:
+        Günümüzde çevre dostu teknolojilere olan talep arttıkça, 
+        elektrikli arabalar piyasadaki önemli bir yer ediniyor.
+        Bu araçlar, fosil yakıt kullanımını azaltarak çevreye daha az zarar veriyor.
+        Ayrıca, elektrikli arabaların tasarımı ve teknolojisi giderek daha çekici ve gelişmiş hale geliyor.`
+};
+
+let araba4 = {
+    img4: "img/img4.jpg",
+    not4: `Elektrikli Arabaların Yükselişi:
+        Günümüzde çevre dostu teknolojilere olan talep arttıkça, 
+        elektrikli arabalar piyasadaki önemli bir yer ediniyor.
+        Bu araçlar, fosil yakıt kullanımını azaltarak çevreye daha az zarar veriyor.
+        Ayrıca, elektrikli arabaların tasarımı ve teknolojisi giderek daha çekici ve gelişmiş hale geliyor.`
+};
+
+let arabalar =[araba1,araba2,araba3,araba4];
+let 
+
+
+function forword(){
+    arabalar.forEach(function(araba) {
+        if(araba <3){
+
+
+        }
+
+    })
+
+};
+
+function back(){
+    
+}
+
+function random(){
+    
+}
