9f9793c37a0467cc621dc0773d524b06adf12c29
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-10-21 15:38:12 +0300

    our rewievs bitti

--- our reviews/app.js
+++ our reviews/app.js
@@ -1,58 +1,65 @@
 let araba1 = {
-    img1: "img/img1.jpg",
-    not1: `Elektrikli Arabaların Yükselişi:
-        Günümüzde çevre dostu teknolojilere olan talep arttıkça, 
-        elektrikli arabalar piyasadaki önemli bir yer ediniyor.
-        Bu araçlar, fosil yakıt kullanımını azaltarak çevreye daha az zarar veriyor.
-        Ayrıca, elektrikli arabaların tasarımı ve teknolojisi giderek daha çekici ve gelişmiş hale geliyor.`
+    img: "img/img1.jpg",
+    not: `Elektrikli Arabaların Yükselişi: Günümüzde çevre dostu teknolojilere olan talep arttıkça, 
+    elektrikli arabalar piyasadaki önemli bir yer ediniyor. Bu araçlar, fosil yakıt kullanımını azaltarak 
+    çevreye daha az zarar veriyor. Ayrıca, elektrikli arabaların tasarımı ve teknolojisi giderek daha çekici 
+    ve gelişmiş hale geliyor.`
 };
 
 let araba2 = {
-    img2: "img/img2.jpg",
-    not2: `Elektrikli Arabaların Yükselişi:
-        Günümüzde çevre dostu teknolojilere olan talep arttıkça, 
-        elektrikli arabalar piyasadaki önemli bir yer ediniyor.
-        Bu araçlar, fosil yakıt kullanımını azaltarak çevreye daha az zarar veriyor.
-        Ayrıca, elektrikli arabaların tasarımı ve teknolojisi giderek daha çekici ve gelişmiş hale geliyor.`
+    img: "img/img2.jpg",
+    not: `Otomatik sürüş teknolojileri, arabaların gelecekteki yönünü şekillendiriyor. 
+    Yapay zeka ve sensör teknolojileri sayesinde, araçlar artık kendi kendine sürüş
+     yapabilir hale geliyor. Bu gelişmeler, 
+    trafik kazalarını azaltabilir ve sürüş deneyimini daha güvenli hale getirebilir.`
 };
 
 let araba3 = {
-    img3: "img/img3.jpg",
-    not3: `Elektrikli Arabaların Yükselişi:
-        Günümüzde çevre dostu teknolojilere olan talep arttıkça, 
-        elektrikli arabalar piyasadaki önemli bir yer ediniyor.
-        Bu araçlar, fosil yakıt kullanımını azaltarak çevreye daha az zarar veriyor.
-        Ayrıca, elektrikli arabaların tasarımı ve teknolojisi giderek daha çekici ve gelişmiş hale geliyor.`
+    img: "img/img3.jpg",
+    not: `Elektrikli arabaların yükselişiyle birlikte, klasik arabaların da özel bir
+     cazibesi var. Klasik arabalar, nostalji sevenler için eşsiz bir çekiciliğe sahip.
+      Bu araçlar, 
+    tarihî değerleri ve özel tasarımlarıyla otomobil tutkunlarını cezbetmeye devam ediyor.`
 };
 
 let araba4 = {
-    img4: "img/img4.jpg",
-    not4: `Elektrikli Arabaların Yükselişi:
-        Günümüzde çevre dostu teknolojilere olan talep arttıkça, 
-        elektrikli arabalar piyasadaki önemli bir yer ediniyor.
-        Bu araçlar, fosil yakıt kullanımını azaltarak çevreye daha az zarar veriyor.
-        Ayrıca, elektrikli arabaların tasarımı ve teknolojisi giderek daha çekici ve gelişmiş hale geliyor.`
+    img: "img/img4.jpg",
+    not: `Yenilenebilir enerji kaynakları, 
+    arabaların enerji tüketimini daha sürdürülebilir hale getiriyor. 
+    Güneş enerjisi ve rüzgar enerjisi gibi kaynaklar, elektrikli arabaların
+     şarj edilmesinde kullanılabiliyor. Bu durum, arabaların çevre
+      üzerindeki etkisini azaltarak enerji verimliliğini artırıyor.`
 };
 
-let arabalar =[araba1,araba2,araba3,araba4];
-let 
+let arabalar = [araba1, araba2, araba3, araba4];
+let gosterilenObjectIndex = 0;
 
-
-function forword(){
-    arabalar.forEach(function(araba) {
-        if(araba <3){
-
-
-        }
-
-    })
-
-};
-
-function back(){
-    
+function forword() {
+    if (gosterilenObjectIndex < arabalar.length - 1) {
+        gosterilenObjectIndex++;
+    } else {
+        gosterilenObjectIndex = 0;
+    }
+    gosterilenNesneyiGuncelle();
 }
 
-function random(){
-    
+function back() {
+    if (gosterilenObjectIndex > 0) {
+        gosterilenObjectIndex--;
+    } else {
+        gosterilenObjectIndex = arabalar.length - 1;
+    }
+    gosterilenNesneyiGuncelle();
 }
+
+function random() {
+    gosterilenObjectIndex = Math.floor(Math.random() * arabalar.length);
+    gosterilenNesneyiGuncelle();
+}
+
+function gosterilenNesneyiGuncelle() {
+    let gosterilenAraba = arabalar[gosterilenObjectIndex];
+    document.getElementById("imgElementId").src = gosterilenAraba.img;
+    document.getElementById("notElementId").textContent = gosterilenAraba.not;
+}
+
