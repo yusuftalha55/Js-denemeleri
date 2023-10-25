502afbb419a9ea086bf4ce56948f4aa6612349c9
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-10-25 18:29:42 +0300

    responsive bar 3 bitti

---
+++ 3 responsive bar/app.js
@@ -0,0 +1,18 @@
+let secenekler =[
+    document.getElementById("open1"),
+    document.getElementById("open2"),
+    document.getElementById("open3"),
+    document.getElementById("open4")
+];
+
+function buttonClick(){
+    secenekler.forEach(function(secenek){
+        if(secenek.style.display==="none" || secenek.style.display===""){
+            secenek.style.display="block";
+        }
+        else{
+            secenek.style.display="none";
+        }
+
+    });
+}
