bd9f9a6539e81835d26cdb73d90586bb37799ac6
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-10-25 18:15:14 +0300

    Çalışan bir versiyonu

--- 2 responsive bar/app.js
+++ 2 responsive bar/app.js
@@ -5,15 +5,13 @@ let secenekler = [
     document.getElementById("open4")
 ];
 
-let seceneklerIndex = 0;
-
 function buttonClick() {
-    var elements = document.getElementsByClassName("open");
-    if (elements[seceneklerIndex].style.display="none" || secenekler[seceneklerIndex].style.display === "") {
-        elements[seceneklerIndex].style.display = "block";
-        
-    }  else {
-        elements[seceneklerIndex].style.display="";
-
-    }
+    secenekler.forEach(function (secenek) {
+        if (secenek.style.display === "none" || secenek.style.display === "") {
+            secenek.style.display = "block";
+        } else {
+            secenek.style.display = "none";
+        }
+    });
 }
+
