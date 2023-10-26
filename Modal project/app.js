955c8331145e4781bac19ae7a6f1ff1546fb265d
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-10-26 17:12:52 +0300

    hide show methodu çalışır hali

--- Modal project/app.js
+++ Modal project/app.js
@@ -1,15 +1,15 @@
 function openModal(){
     let modalContent = document.getElementById("model-content");
-    let modalProject = document.getElementsByClassName("modalProject");
+    let hideModal = document.getElementById("hideModal");
     if(modalContent.style.display ==="none" || modalContent.style.display === ""){
         modalContent.style.display ="block";
-        modalProject.style.display ="none";
-        
+        hideModal.style.display ="none";
+
 
     }
     else{
         modalContent.style.display ="none";
-        modalProject.style.display ="block";
+        hideModal.style.display ="block";
     }
 
 }
