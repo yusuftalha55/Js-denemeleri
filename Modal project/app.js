a26024f810479edc483af662d8c3f0707fee9804
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-10-26 17:08:38 +0300

    ufak eklemeler yaptım

--- Modal project/app.js
+++ Modal project/app.js
@@ -1,3 +1,15 @@
 function openModal(){
-    
+    let modalContent = document.getElementById("model-content");
+    let modalProject = document.getElementsByClassName("modalProject");
+    if(modalContent.style.display ==="none" || modalContent.style.display === ""){
+        modalContent.style.display ="block";
+        modalProject.style.display ="none";
+        
+
+    }
+    else{
+        modalContent.style.display ="none";
+        modalProject.style.display ="block";
+    }
+
 }
