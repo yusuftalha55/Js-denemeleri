ab080366e30e41a09aeed501dec6259fbed07dfe
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-10-27 17:53:27 +0300

    modal 2 projesi bitti

--- 2 modal project/app.js
+++ 2 modal project/app.js
@@ -1,3 +1,27 @@
 function openModal(){
-    
+    let secondMain = document.querySelector(".secondMain");
+    let main = document.querySelector(".main");
+    if(secondMain.style.display === "none" || secondMain.style.display === ""){
+        secondMain.style.display = "block";
+        main.style.display = "none";
+    }
+    else{
+        secondMain.style.display = "none";
+        main.style.display = "block";
+    }
+}
+
+function closeModal(){
+    let secondMain = document.querySelector(".secondMain");
+    let main = document.querySelector(".main");
+    if(main.style.display === "none" || main.style.display === ""){
+        secondMain.style.display = "none";
+        main.style.display = "block";
+    }
+    else{
+        secondMain.style.display = "block";
+        main.style.display = "none";
+    }
+
+
 }
