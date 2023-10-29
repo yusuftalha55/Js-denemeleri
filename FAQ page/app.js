6caa2a9c7ae62ce97178675034c1ece560de66e1
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-10-29 18:52:30 +0300

    bir kaç eksik dışında tam halinde

--- FAQ page/app.js
+++ FAQ page/app.js
@@ -7,3 +7,23 @@ function buttonOneClick(){
         pOne.style.display = "none"
     }
 }
+
+function buttonTwoClick(){
+    let pTwo = document.getElementById("pTwo");
+    if(pTwo.style.display === "none"){
+        pTwo.style.display = "block";
+    }
+    else{
+        pTwo.style.display = "none"
+    }
+}
+
+function buttonThreeClick(){
+    let pThree = document.getElementById("pThree");
+    if(pThree.style.display === "none"){
+        pThree.style.display = "block";
+    }
+    else{
+        pThree.style.display = "none"
+    }
+}
