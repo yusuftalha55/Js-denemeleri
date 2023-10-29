abf585186738469fa85f82b1581b02c4eb4284ab
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-10-29 18:49:03 +0300

    projenin ana hatları tamamlandı

---
+++ FAQ page/app.js
@@ -0,0 +1,9 @@
+function buttonOneClick(){
+    let pOne = document.getElementById("pOne");
+    if(pOne.style.display === "none"){
+        pOne.style.display = "block";
+    }
+    else{
+        pOne.style.display = "none"
+    }
+}
