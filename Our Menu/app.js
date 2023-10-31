21156bd3b8522ae9df8fde34925710a52d3ec180
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-10-31 14:16:07 +0300

    ufak düzeltme

--- Our Menu/app.js
+++ Our Menu/app.js
@@ -1,12 +1,16 @@
+let yumurta = document.getElementsByClassName("yumurta");
+let menemen = document.getElementsByClassName("menemen");
+let meyveSalatası = document.getElementsByClassName("meyveSalatası");
+let gununCorbasi = document.getElementsByClassName("gununCorbasi");
+let cay = document.getElementsByClassName("cay");
+let portakalSuyu = document.getElementsByClassName("portakalSuyu");
+let mercimekCorbası = document.getElementsByClassName("mercimekCorbası");
+let biftek = document.getElementsByClassName("biftek");
+
+
 function allClick(){
-    let yumurta = document.getElementsByClassName("yumurta");
-    let menemen = document.getElementsByClassName("menemen");
-    let meyveSalatası = document.getElementsByClassName("meyveSalatası");
-    let gununCorbasi = document.getElementsByClassName("gununCorbasi");
-    let cay = document.getElementsByClassName("cay");
-    let portakalSuyu = document.getElementsByClassName("portakalSuyu");
-    let mercimekCorbası = document.getElementsByClassName("mercimekCorbası");
-    let biftek = document.getElementsByClassName("biftek");
+    
+
 
 }
 
