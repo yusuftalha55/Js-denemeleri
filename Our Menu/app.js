6ce42163e33434002469bcdbdea719d67648f45e
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-11-01 15:50:25 +0300

    javascript kısmı bitti

--- Our Menu/app.js
+++ Our Menu/app.js
@@ -8,57 +8,81 @@ let mercimekCorbası = document.getElementById("mercimekCorbası");
 let biftek = document.getElementById("biftek");
 let contents = document.getElementById("contents");
 
-let hepsi =[yumurta,menemen,meyveSalatası,gununCorbasi,
-cay,portakalSuyu,mercimekCorbası,biftek];
+let hepsi = [yumurta, menemen, meyveSalatası, gununCorbasi,
+    cay, portakalSuyu, mercimekCorbası, biftek];
 
 
-yumurta.style.display="none";
-menemen.style.display="none";
-meyveSalatası.style.display="none";
-gununCorbasi.style.display="none";
-cay.style.display="none";
-portakalSuyu.style.display="none";
-mercimekCorbası.style.display="none";
-biftek.style.display="none";
+yumurta.style.display = "none";
+menemen.style.display = "none";
+meyveSalatası.style.display = "none";
+gununCorbasi.style.display = "none";
+cay.style.display = "none";
+portakalSuyu.style.display = "none";
+mercimekCorbası.style.display = "none";
+biftek.style.display = "none";
 
 
 
-function allClick(){
-    yumurta.style.display="block";
-    menemen.style.display="block";
-    meyveSalatası.style.display="block";
-    gununCorbasi.style.display="block";
-    cay.style.display="block";
-    portakalSuyu.style.display="block";
-    mercimekCorbası.style.display="block";
-    biftek.style.display="block";
-    
+function allClick() {
+    yumurta.style.display = "block";
+    menemen.style.display = "block";
+    meyveSalatası.style.display = "block";
+    gununCorbasi.style.display = "block";
+    cay.style.display = "block";
+    portakalSuyu.style.display = "block";
+    mercimekCorbası.style.display = "block";
+    biftek.style.display = "block";
+
 
 }
 
 function breakfastClick() {
-    yumurta.style.display="block";
-    menemen.style.display="block";
+    yumurta.style.display = "block";
+    menemen.style.display = "block";
+    meyveSalatası.style.display = "none";
+    gununCorbasi.style.display = "none";
+    cay.style.display = "none";
+    portakalSuyu.style.display = "none";
+    mercimekCorbası.style.display = "none";
+    biftek.style.display = "none";
 
 
 }
 
 
-function lunchClick(){
-    meyveSalatası.style.display="block";
-    gununCorbasi.style.display="block";
+function lunchClick() {
+    meyveSalatası.style.display = "block";
+    gununCorbasi.style.display = "block";
+    yumurta.style.display = "none";
+    menemen.style.display = "none";
+    cay.style.display = "none";
+    portakalSuyu.style.display = "none";
+    mercimekCorbası.style.display = "none";
+    biftek.style.display = "none";
 
 
 }
 
-function shakesClick(){
-    cay.style.display="block";
-    portakalSuyu.style.display="block";
+function shakesClick() {
+    cay.style.display = "block";
+    portakalSuyu.style.display = "block";
+    yumurta.style.display = "none";
+    menemen.style.display = "none";
+    meyveSalatası.style.display = "none";
+    gununCorbasi.style.display = "none";
+    mercimekCorbası.style.display = "none";
+    biftek.style.display = "none";
 
 }
 
-function dinnerClick(){
-    mercimekCorbası.style.display="block";
-    biftek.style.display="block";
+function dinnerClick() {
+    mercimekCorbası.style.display = "block";
+    biftek.style.display = "block";
+    yumurta.style.display = "none";
+    menemen.style.display = "none";
+    meyveSalatası.style.display = "none";
+    gununCorbasi.style.display = "none";
+    cay.style.display = "none";
+    portakalSuyu.style.display = "none";
 
 }
