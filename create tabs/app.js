cab0cc8ead7c8d2cd5817a3b46632d6256dafbff
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-11-03 16:10:05 +0300

    proje bitti

--- create tabs/app.js
+++ create tabs/app.js
@@ -1,15 +1,18 @@
-function historyClick(){
-
-
+function historyClick() {
+    let contentP = document.getElementById("contentP");
+    contentP.innerHTML = "Romanya operasyonlarını da tahminlerimize dahil ettiğimizde, Ford Otosan'ın 2023 yılında toplam satış hacmindeki %38'lik büyüme, maliyet enflasyonunu yansıtıldığı araç satış fiyat artış varsayımları ve TL'nin € karşısında ortalama %29’luk değer kaybı tahminmizin etkisiyle yıllık bazda %81 artışla 311 milyar TL konsolide gelir elde edeceğini öngörüyoruz. Şirket'in 2023'te, uluslararası gelirlerinin toplam cirodaki payının artması nedeniyle yıllık 1.5 yüzde puan düşüşle %10’luk bir FAVÖK marjı ve yıllık %58 artışla 31.3 milyar TL FAVÖK elde edeceğini hesaplıyoruz.";
 }
 
-function visionClick(){
-
-
-    
+function visionClick() {
+    let contentP = document.getElementById("contentP");
+    contentP.innerHTML = "Erdemir, 1960'ta devlet tarafından Karadeniz Ereğli'de kurulan ve 2006'da özelleştirilerek OYAK'a devredilen demir ve çelik fabrikası. Şirket 11 Mayıs 1960 tarihinde resmen kurulmuş olup fabrika kurulması 42 ay sürmüş ve 15 Mayıs 1965'te işletmeye alınmıştır.";
 }
 
-function goalsClick(){
-    
+
+function goalsClick() {
+    let contentP = document.getElementById("contentP");
+    contentP.innerHTML = "Türk Traktör, 1955 yılında Amerikan Marshall Planı yardımları kapsamında Minneapolis Moline ve Türk kamu kurumları ortaklığıyla Ankara'da kurulan traktör ve ziraat makinaları üreten şirket. 1992'de özelleştirilen şirket, günümüzde Koç Holding ve CNH Industrial ortaklığında faaliyet göstermektedir.";
+
+
 }
 
