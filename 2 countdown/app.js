88cc86bc2c8e8ef7540ba5823282a54ad86bffa2
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-11-04 15:43:55 +0300

    javascritte baya ilerledim

--- 2 countdown/app.js
+++ 2 countdown/app.js
@@ -2,5 +2,16 @@ const targetYear = new Date("jan 1 , 2024  00:00:00").getTime();
 
 function newYear(){
     let now = new Date().getTime();
-    let result = 
+    let result = targetYear - now;
+
+    const second = 1000;
+    const minute = second *60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    let d = Math.floor((result / day));
+    let h = Math.floor((result % day) / hour);
+    let m = Math.floor((result % hour) / minute);
+    let s = Math.floor((result % minute) / second);
+
 }
