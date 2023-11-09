ab587ae293daf150ac820d1d0d39ae9ba26663ee
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-11-09 17:02:43 +0300

    elementlere silme butonu ekledim

--- grocery list/app.js
+++ grocery list/app.js
@@ -6,7 +6,10 @@ let ul = document.getElementById("ul");
 function submitClick(){
     let input = document.getElementById("input").value;
     let inputLi = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerHTML = "sil";
     inputLi.textContent = input;
     ul.appendChild(inputLi);
+    ul.appendChild(button);
 }
 
