963c591745d5c606c6d51264583c5456c194d332
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-11-09 16:43:29 +0300

    biraz js yazdım

---
+++ grocery list/app.js
@@ -0,0 +1,5 @@
+let input = document.getElementById("input");
+let submit = document.getElementById("submit");
+let clearBtn = document.getElementById("clearBtn");
+let container = document.querySelector(".container");
+
