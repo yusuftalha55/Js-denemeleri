215780b4619a9e76b507dd6c46c2577293e559b7
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-11-09 16:56:04 +0300

    ciddi bir ilerleme sağladım

--- grocery list/app.js
+++ grocery list/app.js
@@ -1,5 +1,12 @@
-let input = document.getElementById("input");
 let submit = document.getElementById("submit");
 let clearBtn = document.getElementById("clearBtn");
 let container = document.querySelector(".container");
+let ul = document.getElementById("ul");
+
+function submitClick(){
+    let input = document.getElementById("input").value;
+    let inputLi = document.createElement("li");
+    inputLi.textContent = input;
+    ul.appendChild(inputLi);
+}
 
