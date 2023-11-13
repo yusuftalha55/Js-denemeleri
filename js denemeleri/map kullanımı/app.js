88b2413d86f83b92936f7214ae536058eeb99b38
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-11-13 19:31:05 +0300

    REACT BAŞLANGIÇ

--- js denemeleri/map kullanımı/app.js
+++ js denemeleri/map kullanımı/app.js
@@ -1,8 +1,16 @@
 let arr = [3,4,5,6];
+typeof(arr(0));
 
 let modifiedArr = arr.map(function(element){
     return element *3;
 
 });
 
+
+
 console.log(modifiedArr);
+
+arr.forEach(function(ar){
+    ar *= 3;
+
+})
