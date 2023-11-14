89bf7ff95508bf621e48c97279e07324273e1a35
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-11-14 16:06:45 +0300

    true false olmasına göre renk verdirdik

---
+++ React/REACT/src/Prodatc.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Products() {
+  const urunler = [
+    { title: "Cabbage", isFruit: false, id: 1 },
+    { title: "Garlic", isFruit: false, id: 2 },
+    { title: "Apple", isFruit: true, id: 3 },
+  ];
+
+  const listItems = urunler.map((product) => (
+    <li key={product.id} style={{ color: product.isFruit ? "green" : "red" }}>
+      {product.title}
+    </li>
+  ));
+
+  return <ul>{listItems}</ul>;
+}
+
+export default Products;
--- React/REACT/src/main.jsx
+++ React/REACT/src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import MyButton from "./MyButton.jsx";
 import AboutPage from "./AboutPage.jsx";
+import Products from "./Prodatc.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <div>
+    <Products></Products>
+
     {/* <AboutPage></AboutPage> */}
 
     {/* <h1>Welcome to my app</h1>
