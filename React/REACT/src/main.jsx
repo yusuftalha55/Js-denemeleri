fa1508d12ab0a0476d59ea311c21b0797fdb62e9
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-11-14 15:44:50 +0300

    yedekleme

--- React/REACT/src/main.jsx
+++ React/REACT/src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <div>
-    <AboutPage></AboutPage>
+    {/* <AboutPage></AboutPage> */}
 
     {/* <h1>Welcome to my app</h1>
     <MyButton></MyButton> */}
