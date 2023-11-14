364cf346d3ba7b23525bf2252bfbb06b662a88ff
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-11-14 15:31:40 +0300

    butonu tekrar aktif hale getirdim

--- React/REACT/src/main.jsx
+++ React/REACT/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import MyButton from "./MyButton.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div>
+    <h1>Welcome to my app</h1>
+    <MyButton></MyButton>
+
+    {/* <App /> */}
+  </div>
+);
