06862b16c47f2d8c533b6594a0ec3df42d0e8f44
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-11-17 17:30:46 +0300

    başlangıç

--- React/REACT/src/ticTacToe.jsx
+++ React/REACT/src/ticTacToe.jsx
@@ -1,3 +1,9 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-
+function Board({ xIsNext, squares, onPlay }) {
+  function handleClick(i) {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+  }
+}
