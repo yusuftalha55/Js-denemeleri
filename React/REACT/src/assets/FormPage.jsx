517ee8512c8ab4fc85bb335b4216fb775b74c168
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-11-18 17:51:27 +0300

    ana taslağını yazdım projenin

---
+++ React/REACT/src/assets/FormPage.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function form(){
+    <div style={{width: 750 }}>
+        <div className="header">
+            <h2>Form in React</h2>
+        </div>
+        <div className="main">
+            <div className="container">
+                <div className="mainInput">
+                    <h3>First Name*</h3>
+                    <input type="text" />
+                    <br></br>
+
+                    <h3>Last Name*</h3>
+                    <input type="text" />
+                    <br></br>
+
+                    <h3>Enter Email*</h3>
+                    <input type="text" />
+                    <br></br>
+
+                    <h3>Contact*</h3>
+                    <input type="number" />
+                    <br></br>
+
+                </div>
+
+
+            </div>
+
+        </div>
+    </div>
+}
--- React/REACT/src/main.jsx
+++ React/REACT/src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Game from "./ticTacToe";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <div>
-    <Game></Game>
+    
     
   </div>
 );
