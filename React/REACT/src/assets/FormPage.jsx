f6d73e738b5949478a987ee95ae3154d73fa97d5
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-11-18 18:01:34 +0300

    proje bitti

--- React/REACT/src/assets/FormPage.jsx
+++ React/REACT/src/assets/FormPage.jsx
@@ -1,34 +1,41 @@
 import React from "react";
 
-function form(){
-    <div style={{width: 750 }}>
+function Form() {
+    return(  <div style={{ width: 750 , textAlign:"center", backgroundColor:"green" }}>
         <div className="header">
-            <h2>Form in React</h2>
+          <h2>Form in React</h2>
         </div>
         <div className="main">
-            <div className="container">
-                <div className="mainInput">
-                    <h3>First Name*</h3>
-                    <input type="text" />
-                    <br></br>
-
-                    <h3>Last Name*</h3>
-                    <input type="text" />
-                    <br></br>
-
-                    <h3>Enter Email*</h3>
-                    <input type="text" />
-                    <br></br>
-
-                    <h3>Contact*</h3>
-                    <input type="number" />
-                    <br></br>
-
-                </div>
-
-
+          <div className="container">
+            <div className="mainInput">
+              <h3>First Name*</h3>
+              <input type="text" />
+              <br></br>
+    
+              <h3>Last Name*</h3>
+              <input type="text" />
+              <br></br>
+    
+              <h3>Enter Email*</h3>
+              <input type="text" />
+              <br></br>
+    
+              <h3>Contact*</h3>
+              <input type="number" />
+              <br></br>
             </div>
-
+    
+            <div className="checkBox">
+              <input type="checkbox" name="Male" />
+              <p>Male</p>
+              <input type="checkbox" name="Female" />
+              <p>Female</p>
+              <input type="checkbox" name="Other" />
+              <p>Other</p>
+            </div>
+          </div>
         </div>
-    </div>
+      </div>)
 }
+
+export default Form;
--- React/REACT/src/main.jsx
+++ React/REACT/src/main.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Form from "./assets/FormPage";
 import "./index.css";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <div>
-    
+    <Form></Form>
     
   </div>
 );
