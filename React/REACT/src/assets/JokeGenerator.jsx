f3bc4662af737dc582d41ecee91aff21526822f3
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-11-21 14:43:12 +0300

    proje bitti

--- React/REACT/src/assets/JokeGenerator.jsx
+++ React/REACT/src/assets/JokeGenerator.jsx
@@ -1,31 +1,29 @@
-import React from "react";
-
+import React, { useState, useRef } from "react";
 
 function JokeGenerator() {
-    const Joke = () => {
-        const[Joke, setJoke] = React.useState("");
-    }
+  const [joke, setJoke] = useState("");
+  const jokeHtml = useRef(null);
 
-    const fetchApi = () => {
-        fetch("https://sv443.net/jokeapi/v2/joke/Programming?type=single")
-        .then((res) => res.json())
-        .then((data) => setJoke(data.Joke));
-    };  
-return (
-    <div style={{
-        textAlign:"center"
-    }}>
+  const fetchApi = () => {
+    fetch("https://sv443.net/jokeapi/v2/joke/Programming?type=single")
+      .then((res) => res.json())
+      .then((data) => {
+        setJoke(data.joke);
+        jokeHtml.current.innerText = data.joke;
+      });
+  };
+
+  return (
+    <div style={{ margin:350 }}>
       <div className="title">
         <h1>JokeGenerator</h1>
       </div>
       <div className="button">
-        <button>Click to generate a joke.</button>
+        <button onClick={fetchApi}>Click to generate a joke.</button>
       </div>
-      <h3 ref={jokeHtml} className="joke">
-
-      </h3>
+      <h3 ref={jokeHtml} className="joke"></h3>
     </div>
   );
 }
 
-export default Joke;
+export default JokeGenerator;
--- React/REACT/src/main.jsx
+++ React/REACT/src/main.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import JokeGenerator from "./assets/JokeGenerator";
 import "./index.css";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <div>
-    
+    <JokeGenerator></JokeGenerator>
     
     
   </div>
