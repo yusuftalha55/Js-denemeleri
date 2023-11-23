84cfb48cea5dc7c5b7a7d1a25bbb96699f0fcb57
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-11-23 19:39:23 +0300

    başka bir deneme

--- React/REACT/src/assets/PasswordValidator.jsx
+++ React/REACT/src/assets/PasswordValidator.jsx
@@ -1,33 +1,36 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 
 function PasswordCheck() {
-    const [password, setPassword] = useState(""); 
-    function inputCheck(){
-        if(password.current.length >=6 && typeof password.current === "string" && typeof password.current === "number" ){
-            "Is strong password"
-        }
-        else{
-           "Is not strong password"
-        }
+  const [password, setPassword] = useState(""); 
 
-
+  function inputCheck() {
+    if (password.length >= 6 && typeof password === "number" && typeof password === "string") {
+      return "Is strong password";
+    } else {
+      return "Is not strong password";
     }
+  }
+
   return (
     <div className="main">
       <div className="title">
-        <h1>checking Password Strenght in ReactJS</h1>
+        <h1>Checking Password Strength in ReactJS</h1>
       </div>
       <div className="input">
         <h3>Enter Password: </h3>
-        <input onChange={(e) => {
-            setPassword(e.target.value)
-        }} type="text" />
+        <input 
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }} 
+          type="text" 
+        />
       </div>
       <div className="check">
-        <h3 style={{color:"red"}}>{inputCheck()}</h3>
+        <h3 style={{ color: "red" }}>{inputCheck()}</h3>
       </div>
     </div>
   );
 }
 
 export default PasswordCheck;
+
