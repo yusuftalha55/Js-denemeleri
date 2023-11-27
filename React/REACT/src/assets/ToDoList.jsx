01a54259a09b2dc39166bede27be04c051887f57
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-11-27 15:57:36 +0300

    todo listin html i tamam sayılır

---
+++ React/REACT/src/assets/ToDoList.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+return(
+    <div className="main">
+        <div className="title">
+            <h1>TODO LIST</h1>
+        </div>
+
+        <div className="inputArea">
+            <input type="text" style={{width:500 , height:175}}/>
+        </div>
+
+        <div className="addArea">
+            <button style={{backgroundColor:"black"}}>ADD</button>
+        </div>
+
+        <div className="data">
+
+        </div>
+
+    </div>
+)
--- React/REACT/src/main.jsx
+++ React/REACT/src/main.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import PasswordControl from "./assets/PasswordValidator4";
 import "./index.css";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <div>
-     <PasswordControl></PasswordControl>
+     
   </div>
 );
