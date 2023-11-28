f0da9862afc1f81413618744d75a43c8303ec4a0
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-11-28 18:08:36 +0300

    programın bitmeye yakın hali

--- React/REACT/src/assets/ToDoList.jsx
+++ React/REACT/src/assets/ToDoList.jsx
@@ -1,22 +1,46 @@
 import React from "react";
 
-return(
-    <div className="main">
+const TodoList = () => {
+    const [task, setTask] = useState("");
+    const [tasks, setTasks] = useState([]);
+  
+    const addTask = () => {
+      if (task.trim() !== "") {
+        setTasks([...tasks, task]);
+        setTask("");
+      }
+    };
+  
+    return (
+      <div className="main">
         <div className="title">
-            <h1>TODO LIST</h1>
+          <h1>TODO LIST</h1>
         </div>
-
+  
         <div className="inputArea">
-            <input type="text" style={{width:500 , height:175}}/>
+          <input
+            type="text"
+            style={{ width: 500, height: 30 }}
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+          />
         </div>
-
+  
         <div className="addArea">
-            <button style={{backgroundColor:"black"}}>ADD</button>
+          <button style={{ backgroundColor: "black" }} onClick={addTask}>
+            ADD
+          </button>
         </div>
-
+  
         <div className="data">
-
+          <ul>
+            {tasks.map((task, index) => (
+              <li key={index}>{task}</li>
+            ))}
+          </ul>
         </div>
-
-    </div>
-)
+      </div>
+    );
+  };
+  
+  export default TodoList;
