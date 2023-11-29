be9fc273dee34c09d0f44d79190ff0a43aa51981
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-11-29 15:35:55 +0300

    Proje bitti

--- React/REACT/src/assets/ToDoList.jsx
+++ React/REACT/src/assets/ToDoList.jsx
@@ -1,45 +1,47 @@
-import React, {useState, useRef} from "react";
+import React, { useState, useRef } from "react";
 
-const TodoList = () =>{
-	const listRef = useRef("");
-	const inputValue = useRef("");
+const TodoList = () => {
+  const listRef = useRef(null);
+  const inputValue = useRef(null);
 
-	const adToDo = () => {
-		const newTask = document.createElement("li");
-		const newTaskDel = document.createElement("button");
-		newTaskDel.addEventListener("click", delClick);
-		newTaskDel.textContent = "Sil";
-		newTask.appendChild(newTaskDel);
+  const adToDo = () => {
+    const newTask = document.createElement("li");
+    const newTaskDel = document.createElement("button");
+    newTaskDel.addEventListener("click", delClick);
+    newTaskDel.textContent = "Sil";
 
-		newTask.innerHTML = inputValue.current.value;
+    newTask.innerHTML = inputValue.current.value;
+    newTask.appendChild(newTaskDel);
 
-		listRef.appendChild(newTask);
+    listRef.current.appendChild(newTask);
+  };
 
-	}
-}
+  const delClick = (e) => {
+    const taskToRemove = e.target.parentNode;
+    taskToRemove.remove();
+  };
 
-return(
-    <div className="main">
-        <div className="title">
-            <h1>TODO LIST</h1>
-        </div>
+  return (
+    <div  style={{margin:350 , backgroundColor:"#F0F8FF", width:750 , height:750}} className="main">
+      <div className="title">
+        <h1>TODO LIST</h1>
+      </div>
 
-        <div className="inputArea">
-            <input ref={inputValue} type="text" style={{width:500 , height:175}}/>
-        </div>
+      <div className="inputArea">
+        <input placeholder="add item..." ref={inputValue} type="text" style={{ width: 150, height: 45 }} />
+      </div>
 
-        <div className="addArea">
-            <button onClick={adToDo} style={{backgroundColor:"black"}}>ADD</button>
-        </div>
+      <div style={{margin:15}} className="addArea">
+        <button onClick={adToDo} style={{ backgroundColor: "grey" }}>
+          ADD
+        </button>
+      </div>
 
-        <div className="data">
-			<ul ref={listRef}>
-
-			</ul>
-
-        </div>
-
+      <div className="data">
+        <ul ref={listRef}></ul>
+      </div>
     </div>
-) 
-  
-  export default TodoList;
+  );
+};
+
+export default TodoList;
