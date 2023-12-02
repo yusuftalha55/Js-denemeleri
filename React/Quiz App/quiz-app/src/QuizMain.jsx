c2d3c5ae3b7a0bebbe06ad4408c26c4f07cb12e8
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-12-02 18:22:49 +0300

    proje başlangıcı

--- React/Quiz App/quiz-app/src/QuizMain.jsx
+++ React/Quiz App/quiz-app/src/QuizMain.jsx
@@ -1,2 +1,23 @@
 import React from "react";
 
+const QuizApp = () => {
+  return (
+    <div className="main">
+      <div className="title">
+        <h1>QUIZ APP</h1>
+      </div>
+      <div className="questions-title">
+        <h3>Question 1</h3>
+        <h2 className="questions">What is the capital of Haryana?</h2>
+        <input
+          type="radio"
+          value="option1"
+          checked={selectedOption === "option1"}
+          onChange={() => setSelectedOption("option1")}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default QuizApp
--- React/Quiz App/quiz-app/src/index.js
+++ React/Quiz App/quiz-app/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import QuizApp from './QuizMain';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
+    <QuizApp></QuizApp>
   </React.StrictMode>
 );
 
