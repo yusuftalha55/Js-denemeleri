b8857caaf10e258fd88495ba5d69950a6e938a73
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-12-06 18:19:28 +0300

    yanlış kullanımlar düzeltildi

--- React/Quiz App2/QuizApp2/src/assets/QuizAppMain.jsx
+++ React/Quiz App2/QuizApp2/src/assets/QuizAppMain.jsx
@@ -1,63 +1,64 @@
-import React,{useState, useRef} from "react";
+import React, { useState, useRef } from "react";
 import qBank from "./Qbank";
 import questionValues from "./QuestionsValue";
 
-const quizApp = () => {
-    const [selectedAnswer, setSelectedAnswer] = useState("");
-    const whichQ = useRef();
-    const question = useRef();
-    const answer1 = useRef();
-    const answer2 = useRef();
-    const answer3 = useRef();
-    const answer4 = useRef();
+const QuizApp = () => {
+  const [selectedAnswer, setSelectedAnswer] = useState("");
+  const whichQ = useRef();
+  const questionRef = useRef();
+  const answer1Ref = useRef();
+  const answer2Ref = useRef();
+  const answer3Ref = useRef();
+  const answer4Ref = useRef();
 
-    return(
-        <div className="main">
-            <div className="title">
-                <h1>QUIZ APP</h1>
-            </div>
-            <div className="questions">
-                <div className="whichQ">
-                    <h2 ref={whichQ}></h2>
-                </div>
-                <div className="question">
-                    <h3 ref={question}></h3>
-                </div>
-                <div className="answers">
-                    <input type="radio" 
-                    value="answer1"
-                    checked = {selectedAnswer === answer1}
-                    onChange={() = selectedAnswer("answer1")}
-                    />
-                    <h4 ref={answer1}></h4>
-
-                    <input type="radio" 
-                    value="answer2"
-                    checked = {selectedAnswer === answer2}
-                    onChange={() = selectedAnswer("answer2")}
-                    />
-                    <h4 ref={answer2}></h4>
-
-                    <input type="radio" 
-                    value="answer3"
-                    checked = {selectedAnswer === answer3}
-                    onChange={() = selectedAnswer("answer3")}
-                    />
-                    <h4 ref={answer3}></h4>
-
-                    <input type="radio" 
-                    value="answer4"
-                    checked = {selectedAnswer === answer4}
-                    onChange={() = selectedAnswer("answer4")}
-                    />
-                    <h4 ref={answer4}></h4>
-                    
-
-                </div>
-
-            </div>
-
+  return (
+    <div className="main">
+      <div className="title">
+        <h1>QUIZ APP</h1>
+      </div>
+      <div className="questions">
+        <div className="whichQ">
+          <h2 ref={whichQ}></h2>
         </div>
-    )
+        <div className="question">
+          <h3 ref={questionRef}></h3>
+        </div>
+        <div className="answers">
+          <input
+            type="radio"
+            value="answer1"
+            checked={selectedAnswer === "answer1"}
+            onChange={() => setSelectedAnswer("answer1")}
+          />
+          <h4 ref={answer1Ref}></h4>
 
-}
+          <input
+            type="radio"
+            value="answer2"
+            checked={selectedAnswer === "answer2"}
+            onChange={() => setSelectedAnswer("answer2")}
+          />
+          <h4 ref={answer2Ref}></h4>
+
+          <input
+            type="radio"
+            value="answer3"
+            checked={selectedAnswer === "answer3"}
+            onChange={() => setSelectedAnswer("answer3")}
+          />
+          <h4 ref={answer3Ref}></h4>
+
+          <input
+            type="radio"
+            value="answer4"
+            checked={selectedAnswer === "answer4"}
+            onChange={() => setSelectedAnswer("answer4")}
+          />
+          <h4 ref={answer4Ref}></h4>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default QuizApp;
