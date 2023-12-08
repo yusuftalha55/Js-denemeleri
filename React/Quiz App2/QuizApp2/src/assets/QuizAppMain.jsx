e76a84fcaefb6d187fe79222e6b5c5f22db26cda
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-12-08 16:34:22 +0300

    son haline yakın bir kaç hata kaldı düzeltilmesi gereken

--- React/Quiz App2/QuizApp2/src/assets/QuizAppMain.jsx
+++ React/Quiz App2/QuizApp2/src/assets/QuizAppMain.jsx
@@ -5,9 +5,10 @@ import questionValues from "./QuestionsValue";
 const QuizApp = () => {
   const [selectedAnswer, setSelectedAnswer] = useState("");
   const [score, setScore] = useState("0");
-  const [selectedQuestionId, setSelectedQuestionId] = useState("");
-  const [whichQNumber, setWhichQNumber] = useState(0);
-  const currentQuestionValues = questionValues();
+  const [selectedQuestionId, setSelectedQuestionId] = useState(1);
+  const [whichQNumber, setWhichQNumber] = useState(2);
+
+  const currentQuestionValues = questionValues(selectedQuestionId);
   const whichQ = useRef();
   const questionRef = useRef();
   const answer1Ref = useRef();
@@ -16,36 +17,30 @@ const QuizApp = () => {
   const answer4Ref = useRef();
 
   const submitClick = () => {
-    setSelectedQuestionId(currentQuestionValues.id);
+    setSelectedQuestionId(selectedQuestionId + 1);
     setWhichQNumber(whichQNumber + 1);
     whichQ.current.innerText = `Question ${whichQNumber}`;
 
-    const currentQuestion = questionValues(currentQuestionValues.id);
+    const currentQuestion = questionValues(selectedQuestionId);
     questionRef.current.innerText = currentQuestion.questionText;
 
     answer1Ref.current.innerText = currentQuestion.options[0];
     answer2Ref.current.innerText = currentQuestion.options[1];
     answer3Ref.current.innerText = currentQuestion.options[2];
     answer4Ref.current.innerText = currentQuestion.options[3];
-    
-
-    
-  
-
-    
-  }
+  };
 
   return (
-    <div className="main">
+    <div style={{textAlign:"center", marginLeft:500}} className="main">
       <div className="title">
         <h1>QUIZ APP</h1>
       </div>
       <div className="questions">
         <div className="whichQ">
-          <h2 ref={whichQ}></h2>
+          <h2 ref={whichQ}>Questions 1</h2>
         </div>
         <div className="question">
-          <h3 ref={questionRef}></h3>
+          <h3 ref={questionRef}>What is the capital of Haryana?</h3>
         </div>
         <div className="answers">
           <input
@@ -54,7 +49,7 @@ const QuizApp = () => {
             checked={selectedAnswer === "answer1"}
             onChange={() => setSelectedAnswer("answer1")}
           />
-          <h4 ref={answer1Ref}></h4>
+          <h4 ref={answer1Ref}>Yamunanagar</h4>
 
           <input
             type="radio"
@@ -62,7 +57,7 @@ const QuizApp = () => {
             checked={selectedAnswer === "answer2"}
             onChange={() => setSelectedAnswer("answer2")}
           />
-          <h4 ref={answer2Ref}></h4>
+          <h4 ref={answer2Ref}>Panipat</h4>
 
           <input
             type="radio"
@@ -70,7 +65,7 @@ const QuizApp = () => {
             checked={selectedAnswer === "answer3"}
             onChange={() => setSelectedAnswer("answer3")}
           />
-          <h4 ref={answer3Ref}></h4>
+          <h4 ref={answer3Ref}>Gurgaon</h4>
 
           <input
             type="radio"
@@ -78,7 +73,7 @@ const QuizApp = () => {
             checked={selectedAnswer === "answer4"}
             onChange={() => setSelectedAnswer("answer4")}
           />
-          <h4 ref={answer4Ref}></h4>
+          <h4 ref={answer4Ref}>Chandigarh</h4>
         </div>
         <div className="submit">
           <button onClick={submitClick}>SUBMIT</button>
--- React/Quiz App2/QuizApp2/src/main.jsx
+++ React/Quiz App2/QuizApp2/src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import QuizApp from './assets/QuizAppMain'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
+    <QuizApp></QuizApp>
   </React.StrictMode>,
 )
