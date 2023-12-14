6652634aa7fc0bf15efda7326cf522a4b391f619
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-12-14 17:07:47 +0300

    html kısmının temeli bitti

--- React/FinalQuizApp/EndQuizApp/src/App.jsx
+++ React/FinalQuizApp/EndQuizApp/src/App.jsx
@@ -1,3 +1,48 @@
 import React from "react";
 
-const 
+const App = () => {
+  const submitClick = () => {
+    
+  }
+
+
+
+  return (
+    <div style={{backgroundColor:Bisque}}>
+      <div className="title">QUIZ APP</div>
+      <div className="questions">
+        <div className="whichQuestion">
+          <h2></h2>
+        </div>
+        <div className="contentQuestion">
+          <h3></h3>
+        </div>
+        <div className="answers">
+          <div className="answer1">
+            <input type="radio" />
+            <h4></h4>
+          </div>
+          <div className="answer2">
+            <input type="radio" />
+            <h4></h4>
+          </div>
+          <div className="answer3">
+            <input type="radio" />
+            <h4></h4>
+          </div>
+          <div className="answer4">
+            <input type="radio" />
+            <h4></h4>
+          </div>
+        </div>
+        <div className="submit">
+          <button onClick={submitClick}>Submit</button>
+        </div>
+
+      </div>
+    </div>
+  )
+
+}
+
+export default App
