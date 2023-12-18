b5ae8913fe2b68cdc1133c73b4cc9e98609c3187
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-12-18 17:17:06 +0300

    html kısmı halledildi ufak bir css eklendi

--- React/COIN FLIP/coinFlip/src/App.jsx
+++ React/COIN FLIP/coinFlip/src/App.jsx
@@ -2,7 +2,24 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  return(
+    <div className='main'>
+      <div className='title'>
+        <h2>Hadi Yazı Tura Atalım</h2>
+      </div>
+      <div className='image'>
+        <img src="" alt="para gösterilemedi" />
+      </div>
+      <div className='button'>
+        <h3>Bas ve Gör</h3>
+      </div>
+      <div className='result'>
+        <p>{}  kere yazı tura atıldı, {} kere yazı geldi, {} kere tura geldi.</p>
+
+      </div>
+
+    </div>
+  )
 }
 
 export default App
