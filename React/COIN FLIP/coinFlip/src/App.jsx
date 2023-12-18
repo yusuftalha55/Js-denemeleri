ccab5713ac7149ac793e07e40c63068171788276
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-12-18 17:18:52 +0300

    html kısmı tam anlamıyla bitti sadece js kaldı

--- React/COIN FLIP/coinFlip/src/App.jsx
+++ React/COIN FLIP/coinFlip/src/App.jsx
@@ -8,10 +8,10 @@ function App() {
         <h2>Hadi Yazı Tura Atalım</h2>
       </div>
       <div className='image'>
-        <img src="" alt="para gösterilemedi" />
+        <img src="src\assets\img\1TL_obverse.png" alt="para gösterilemedi" />
       </div>
       <div className='button'>
-        <h3>Bas ve Gör</h3>
+        <button>Bas ve Gör</button>
       </div>
       <div className='result'>
         <p>{}  kere yazı tura atıldı, {} kere yazı geldi, {} kere tura geldi.</p>
