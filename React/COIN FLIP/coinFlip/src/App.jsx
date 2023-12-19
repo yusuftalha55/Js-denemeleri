512c18c11944b4eac469ce5d29d237b3fbc9e196
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-12-19 17:44:39 +0300

    proje bitti

--- React/COIN FLIP/coinFlip/src/App.jsx
+++ React/COIN FLIP/coinFlip/src/App.jsx
@@ -1,45 +1,46 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
   const [flipNumber, setFlipNumber] = useState(0);
   const [yaziNumber, setYaziNumber] = useState(0);
   const [turaNumber, setTuraNumber] = useState(0);
   const [coinSide, setCoinSide] = useState();
-  const binaryValue = Math.round(Math.random());
-  const [image, setImage] = useState('src/assets/img/1TL_reverse.png')
-
-
+  const [image, setImage] = useState('src/assets/img/1TL_reverse.png');
 
   const flipClick = () => {
+    const binaryValue = Math.round(Math.random());
     setCoinSide(binaryValue);
-    if (coinSide === 1) {
-      setImage('src/assets/img/1TL_obverse.png')
-    }
-    else{
-      setImage('src/assets/img/1TL_reverse.png')
+
+    if (binaryValue === 1) {
+      setYaziNumber((prevYaziNumber) => prevYaziNumber + 1);
+      setImage('src/assets/img/1TL_obverse.png');
+    } else {
+      setTuraNumber((prevTuraNumber) => prevTuraNumber + 1);
+      setImage('src/assets/img/1TL_reverse.png');
     }
 
-    
-  }
-  return(
+    setFlipNumber((prevFlipNumber) => prevFlipNumber + 1);
+  };
+
+  return (
     <div className='main'>
       <div className='title'>
         <h2>Hadi Yazı Tura Atalım</h2>
       </div>
       <div className='image'>
-        <img src={image} alt="para gösterilemedi" />
+        <img src={image} alt='para gösterilemedi' />
       </div>
       <div className='button'>
         <button onClick={flipClick}>Bas ve Gör</button>
       </div>
       <div className='result'>
-        <p>{flipNumber}  kere yazı tura atıldı, {yaziNumber} kere yazı geldi, {turaNumber} kere tura geldi.</p>
-
+        <p>
+          {flipNumber} kere yazı tura atıldı, {yaziNumber} kere yazı geldi, {turaNumber} kere tura geldi.
+        </p>
       </div>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
