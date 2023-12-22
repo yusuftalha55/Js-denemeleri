af311630e1a1b862fbe67ffc5739baca13cbf08e
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-12-22 16:39:18 +0300

    resim düzenlemeleri yaptım

--- React/FoodRecipe/src/ContainerBreakfast.jsx
+++ React/FoodRecipe/src/ContainerBreakfast.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import './Container.css'
-import makarna from './assets/img/makarna.jpg'; 
+import yumurta from './assets/img/yumurta.jpg'; 
 
 const ContainerBreakfast = () => {
     return(
         <div className="mainContainer">
-            <h2 className="title">Some Meal</h2>
+            <h2 className="title">Yumurta</h2>
             <ul>
                 <li>Lezzetli: Sahanda yumurta, yumurtanın doğal lezzetini korur ve altın renginde kızarmış kenarlarıyla lezzetli bir seçenektir.</li>
                 <li>Kolay ve Hızlı Hazırlık: Sahanda yumurta, hızlı bir kahvaltı veya pratik bir öğle yemeği seçeneği olarak kolayca hazırlanabilir.</li>
                 <li>Mükemmel Protein Kaynağı: Sahanda yumurta, yüksek kaliteli protein içerir ve vücut için gerekli olan amino asitleri sağlar.</li>
                 <li>Çeşitli Sunum Olanakları: Sahanda yumurta, çeşitli malzemeler ve baharatlar eklenerek farklı lezzetler elde edilebilecek esnek bir temel yemektir.</li>
             </ul>
-            <img className="image" src={makarna} alt="Resim gösterilemedi" />
+            <img className="image" src={yumurta} alt="Resim gösterilemedi" />
         </div>
     )
 }
