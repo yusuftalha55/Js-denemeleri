a96a75106df79d2446693197df5e330303339788
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-12-23 17:00:41 +0300

    searchClick eklenmiş hali

--- React/FoodRecipe/src/App.jsx
+++ React/FoodRecipe/src/App.jsx
@@ -1,27 +1,34 @@
-import { useState } from 'react'
-import './App.css'
-import ContainerDrinks from './ContainerDrinks'
-import ContainerBreakfast from './ContainerBreakfast'
-import ContainerDinner from './ContainerDinner'
+import { useState } from "react";
+import "./App.css";
+import ContainerDrinks from "./ContainerDrinks";
+import ContainerBreakfast from "./ContainerBreakfast";
+import ContainerDinner from "./ContainerDinner";
 
 function App() {
+  const [showScreen, setShowScreen] = useState("");
   
+
+
+
+  const searchClick = () => {
+
+  };
 
   return (
     <div>
-      <div className='search'>
-        <input className='input' type="text" />
-        <button className='button'>Ara</button>
+      <div className="search">
+        <input className="input" type="text" />
+        <button className="button" onClick={searchClick}>
+          Ara
+        </button>
       </div>
-      <div className='containers'>
+      <div className="containers">
         <ContainerBreakfast></ContainerBreakfast>
         <ContainerDrinks></ContainerDrinks>
         <ContainerDinner></ContainerDinner>
-        
       </div>
     </div>
-  
-  )
+  );
 }
 
-export default App
+export default App;
