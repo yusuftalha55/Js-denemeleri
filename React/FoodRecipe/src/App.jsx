9534c0ee028c792b1379ce60447d48f69f5cf4a8
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-12-23 17:38:31 +0300

    proje bitti

--- React/FoodRecipe/src/App.jsx
+++ React/FoodRecipe/src/App.jsx
@@ -1,40 +1,48 @@
-import { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./App.css";
 import ContainerDrinks from "./ContainerDrinks";
 import ContainerBreakfast from "./ContainerBreakfast";
 import ContainerDinner from "./ContainerDinner";
 
 function App() {
+  const [searchInput, setSearchInput] = useState("");
   const [showScreen, setShowScreen] = useState();
-  
-  const All = () => {
-    return(
-      <div> 
-      <ContainerBreakfast></ContainerBreakfast>
-      <ContainerDrinks></ContainerDrinks>
-      <ContainerDinner></ContainerDinner>
-      </div>
-      
-    )
-  } 
-  
-
-
+  const containersRef = useRef();
 
   const searchClick = () => {
-
+    const searchInputLower = searchInput.toLowerCase();
+  
+    if (searchInputLower === "breakfast") {
+      setShowScreen(<ContainerBreakfast />);
+    } else if (searchInputLower === "drinks") {
+      setShowScreen(<ContainerDrinks />);
+    } else if (searchInputLower === "dinner") {
+      setShowScreen(<ContainerDinner />);
+    } 
   };
+  
 
   return (
     <div>
       <div className="search">
-        <input className="input" type="text" />
+        <input
+          className="input"
+          type="text"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
         <button className="button" onClick={searchClick}>
           Ara
         </button>
       </div>
-      <div className="containers">
-        <p>{showScreen}</p>        
+      <div ref={containersRef} className="containers">
+        {showScreen || (
+          <>
+            <ContainerBreakfast />
+            <ContainerDrinks />
+            <ContainerDinner />
+          </>
+        )}
       </div>
     </div>
   );
