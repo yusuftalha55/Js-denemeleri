e1fb11002e6dad30b981199c236c1a95ef2c74c0
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-12-24 16:49:38 +0300

    ana teması oluşturuldu oyunun

--- React/Tic-Tac-Toe/src/App.jsx
+++ React/Tic-Tac-Toe/src/App.jsx
@@ -2,6 +2,31 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  return (
+    <div>
+      <div className="status">{getStatus()}</div>
+      <div className="board">
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+      <button className="reset-button" onClick={resetGame}>
+        Oyunu Sıfırla
+      </button>
+    </div>
+  )
 
 }
 
