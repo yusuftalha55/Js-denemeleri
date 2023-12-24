fcc44adba0326ebf5f79d615b2d6e0a56808dfe9
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-12-24 16:42:13 +0300

    youtube dan nasıl çekileceğini bilmediğim için yarım kaldı

--- React/MovieTrailer/src/App.jsx
+++ React/MovieTrailer/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 function App() {
   return (
     <div className='main'>
+      <button className="square">X</button>
       <iframe width="788" height="443"
        src="https://www.youtube.com/embed/YoHD9XEInc0" 
        title="Inception (2010) Official Trailer #1 - Christopher Nolan Movie HD" 
