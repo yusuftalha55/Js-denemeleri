5907fe8c01f7d553697dcd8346ce2160d479c470
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2023-12-24 16:27:28 +0300

    movie trailer programı kurdum

---
+++ React/MovieTrailer/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className='main'>
+      <iframe width="788" height="443"
+       src="https://www.youtube.com/embed/YoHD9XEInc0" 
+       title="Inception (2010) Official Trailer #1 - Christopher Nolan Movie HD" 
+       frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+       allowfullscreen>       
+       </iframe>
+
+    </div>
+  )
+
+}
+
+export default App
