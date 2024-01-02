b4362b5161723b5b3c8792c7c138b51e2602667a
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-01-02 16:47:56 +0300

    handlePLayi yazdım

--- React/Tic-Tac-Toe-Final/src/App.jsx
+++ React/Tic-Tac-Toe-Final/src/App.jsx
@@ -59,3 +59,16 @@ function Board ({ xIsNext, squares, onPlay}) {
   );
 
 }
+
+export default function Game () {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay (nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove +1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+} 
