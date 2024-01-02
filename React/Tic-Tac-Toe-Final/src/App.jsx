d7061fd59cf63370ff5c05e3cb65859e95304aab
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-01-02 16:40:23 +0300

    board kısmı bitti

--- React/Tic-Tac-Toe-Final/src/App.jsx
+++ React/Tic-Tac-Toe-Final/src/App.jsx
@@ -32,4 +32,30 @@ function Board ({ xIsNext, squares, onPlay}) {
     status = " Next Player: " + (xIsNext ? "X" : "O");
   }
 
+  return (
+    <div>
+      <div className='status'>
+        {status}
+      </div>
+      <div className='board-row'>
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(0)} />
+      </div>
+
+      <div className='board-row'>
+        <Square value={squares[3]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(0)} />
+      </div>
+
+      <div className='board-row'>
+        <Square value={squares[6]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(0)} />
+      </div>
+
+    </div>
+  );
+
 }
