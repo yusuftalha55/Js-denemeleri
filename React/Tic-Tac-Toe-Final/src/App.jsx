eef6f020a307dd46a9af66a596d57dd162059aaf
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-01-03 16:38:40 +0300

    game fonksiyonunu yazdım

--- React/Tic-Tac-Toe-Final/src/App.jsx
+++ React/Tic-Tac-Toe-Final/src/App.jsx
@@ -91,7 +91,14 @@ export default function Game () {
     );
   });
 
-
-
-
+  return (
+    <div className='game'>
+      <div className='game-board'>
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className='game-info'>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
 } 
