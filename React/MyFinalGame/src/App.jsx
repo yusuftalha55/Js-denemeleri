f452fb047a7db3c6bb138252cc415924082aeed0
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-01-05 23:13:30 +0300

    htmlin bir kısmını yazdım

--- React/MyFinalGame/src/App.jsx
+++ React/MyFinalGame/src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-  
-
   return (
-    <>
+    <div >
+      <div className="main">
+        <div className="title">
+          <h1>Tic Tac Toe Game</h1>
+        </div>
+        <div className="Game">
+          <div className="">
 
-    </>
-  )
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
