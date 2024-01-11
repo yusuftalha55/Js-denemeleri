eb7528f18d57b9234bc89dd10b2529f2e7f2e9f6
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-01-11 17:01:54 +0300

    ufak düzeltme

--- React/Thinking-in-React/src/App.jsx
+++ React/Thinking-in-React/src/App.jsx
@@ -1,44 +1,38 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [click, setClick] = useState(null)
+  const [click, setClick] = useState(null);
 
   const buttonClick = () => {
-    
-    
     if (click === null) {
-      setClick("X")
+      setClick("X");
+    } else if (click === "X") {
+      setClick(null);
     }
-    else if (click === "X") {
-      setClick(null)
-    }
-
-
-  }
-  
+  };
 
   return (
     <>
-      <div className='main'>
-        <div className='search-bar'>
-          <input id='searchId' type="text" placeholder='Search...'/>
+      <div className="main">
+        <div className="search-bar">
+          <input id="searchId" type="text" placeholder="Search..." />
         </div>
-        <div className='button'>
-          <button id='buttonId' onClick={buttonClick}>{click}</button>
+        <div className="button">
+          <button id="buttonId" onClick={buttonClick}>
+            {click}
+          </button>
         </div>
 
-        <div className='product-table'>
-          <div className='name-price'>
+        <div className="product-table">
+          <div className="name-price">
             <h2>Name</h2>
             <h2>Price</h2>
           </div>
-
         </div>
-
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
