6c0f80a0ea36ca752b5286f6cb5ce1eafa789b05
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-01-17 16:00:19 +0300

    ilerliyorum

--- React/PokemonApi/src/App.jsx
+++ React/PokemonApi/src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Pokemon from "./poke";
 
 function App() {
   
-
   return (
     <>
-      
+      <div className="main">
+        <div className="title">
+          <h1>Pokemon Api</h1>
+        </div>
+        <div className="content">
+          <Pokemon></Pokemon>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
