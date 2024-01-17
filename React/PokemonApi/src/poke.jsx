326f8b88e37fad7167300dd32bc06b0a7537f0f5
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-01-17 16:42:18 +0300

    pokemonnların isimlerini çekmeyi başardım

--- React/PokemonApi/src/poke.jsx
+++ React/PokemonApi/src/poke.jsx
@@ -1,26 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-function Pokemon () {
+function Pokemon() {
+  const [pokemonList, setPokemonList] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
+      const data = await response.json();
+      const firstTenPokemon = data.results.slice(0, 10);
+      setPokemonList(firstTenPokemon);
+    };
 
+    fetchData();
+  }, []);
 
-    return (
-        <div className="main">
-            <div className="container">
-                <div className="title">
-                    <h2></h2>
-                </div>
-                <div className="attributes">
-                    
-
-                </div>
-
-
-            </div>
+  return (
+    <div className="main">
+      <div className="container">
+        <div className="title">
+          {pokemonList.map((pokemon, index) => (
+            <h2 key={index}>{pokemon.name}</h2>
+          ))}
         </div>
-
-    );
-
+        <div className="attributes"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Pokemon;
