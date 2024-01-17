76415d67492e5d2e7086f98419feae88af991b71
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-01-17 16:29:39 +0300

    ilerleme devam ediyor

--- React/PokemonApi/src/poke.jsx
+++ React/PokemonApi/src/poke.jsx
@@ -2,6 +2,25 @@ import React from "react";
 
 function Pokemon () {
 
+
+
+    return (
+        <div className="main">
+            <div className="container">
+                <div className="title">
+                    <h2></h2>
+                </div>
+                <div className="attributes">
+                    
+
+                </div>
+
+
+            </div>
+        </div>
+
+    );
+
 }
 
 export default Pokemon;
