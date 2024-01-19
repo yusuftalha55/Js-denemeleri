7d0a5a09c34bac457c20991539ff79c4299b6c74
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-01-19 16:41:02 +0300

    ufak düzeltme ve eklemeler

--- React/PokemonApi/src/pokeAbilities.jsx
+++ React/PokemonApi/src/pokeAbilities.jsx
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function PokeAbilities() {
-  const [pokemonAbilites, setPokemonAbilities] = useState([]);
+  const [pokemonAbilities, setPokemonAbilities] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
       const data = await response.json();
-      
+      const firstTenPokemonAbilities = data.results.url;
+      setPokemonAbilities(firstTenPokemonAbilities);
     };
   }, []);
 }
