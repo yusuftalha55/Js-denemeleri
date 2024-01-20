df31dde52f90724134cc12f50a544c8099a4c62b
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-01-20 18:12:34 +0300

    yeni companent ekliyorum

--- React/PokemonApi/src/poke.jsx
+++ React/PokemonApi/src/poke.jsx
@@ -32,7 +32,7 @@ function Pokemon() {
                 alt={pokemon.name}
               />
               <h2 className="name">{pokemon.name}</h2>
-              <button onClick={() => handleButtonClick(pokemon.url)}>Go to Abilities</button>
+              <button onClick={() => handleButtonClick(pokemon.url.name)}>Go to Abilities</button>
             </div>
           ))}
         </div>
--- React/PokemonApi/src/pokeAbilities.jsx
+++ React/PokemonApi/src/pokeAbilities.jsx
@@ -2,17 +2,12 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function PokeAbilities() {
-  const [pokemonAbilities, setPokemonAbilities] = useState([]);
+  return (
+    <div className="main">
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch("https://pokeapi.co/api/v2/pokemon/1/");
-      const data = await response.json();
-      const firstTenPokemonAbilities = data.abilities.ability;
-      setPokemonAbilities(firstTenPokemonAbilities);
-    };
-    fetchData();
-  }, []);
+    </div>
+  )
+  
 }
 
 export default PokeAbilities;
