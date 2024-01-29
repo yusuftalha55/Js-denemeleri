40a1431c96a75228e0b10e2156f9b98a16fbc96c
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-01-29 18:03:27 +0300

    en güzel ve düzgün çalışan versiyon

--- React/Doviz Hesaplayici/src/App.jsx
+++ React/Doviz Hesaplayici/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState, useEffect } from 'react'; 
+import { useState, useEffect } from "react";
 
 function App() {
+  const [moneyList, setMoneyList] = useState([""]);
+
   useEffect(() => {
     const fetchData = async () => {
-      const url = 'https://currency-exchange.p.rapidapi.com/listquotes';
+      const url = "https://currency-exchange.p.rapidapi.com/listquotes";
       const options = {
-        method: 'GET',
+        method: "GET",
         headers: {
-          'X-RapidAPI-Key': '2dab30615cmshc5b0542dbd9da36p1f0fd6jsn4fc63c166db4',
-          'X-RapidAPI-Host': 'currency-exchange.p.rapidapi.com'
-        }
+          "X-RapidAPI-Key":
+            "2dab30615cmshc5b0542dbd9da36p1f0fd6jsn4fc63c166db4",
+          "X-RapidAPI-Host": "currency-exchange.p.rapidapi.com",
+        },
       };
 
       try {
         const response = await fetch(url, options);
-        const result = await response.text();
+        const result = await response.json();
+        setMoneyList(result);
         console.log(result);
       } catch (error) {
         console.error(error);
       }
     };
 
-    fetchData(); 
-  }, []); 
+    fetchData();
+  }, []);
 
   return (
     <div>
-      {}
+      {moneyList.map((money, index) => (
+        <h2 key={index}>{money}</h2>
+      ))}
     </div>
   );
 }
 
 export default App;
-
