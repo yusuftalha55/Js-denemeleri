f392e8e9c89855ead648b010b614dc46f04ce1d5
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-01-29 17:40:51 +0300

    ufak denemeler

--- React/Doviz Hesaplayici/src/App.jsx
+++ React/Doviz Hesaplayici/src/App.jsx
@@ -1,14 +1,35 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from 'react'; // useEffect eklenmiş
 
 function App() {
-  
+  useEffect(() => {
+    const fetchData = async () => {
+      const url = 'https://currency-exchange.p.rapidapi.com/listquotes';
+      const options = {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Key': '2dab30615cmshc5b0542dbd9da36p1f0fd6jsn4fc63c166db4',
+          'X-RapidAPI-Host': 'currency-exchange.p.rapidapi.com'
+        }
+      };
+
+      try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        console.log(result);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData(); // fetchData fonksiyonunu çağır
+  }, []); // useEffect'i bağımlılıksız olarak tanımla
 
   return (
-   <div>
-    
-   </div>
-  )
+    <div>
+      {/* JSX içeriği buraya ekleyebilirsiniz */}
+    </div>
+  );
 }
 
-export default App
+export default App;
+
