3f187975313d68ed23d3ec8e90bd66973913c16c
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-01-30 17:10:26 +0300

    projenin api nı değiştirdim

--- React/Doviz Hesaplayici/src/App.jsx
+++ React/Doviz Hesaplayici/src/App.jsx
@@ -8,22 +8,19 @@ function App() {
 
   useEffect(() => {
     const fetchData = async () => {
-      const url = "https://currency-exchange.p.rapidapi.com/listquotes";
+      const url = "https://currency-conversion-and-exchange-rates.p.rapidapi.com/latest?from=USD&to=EUR%2CGBP";
       const options = {
         method: "GET",
         headers: {
           "X-RapidAPI-Key":
             "2dab30615cmshc5b0542dbd9da36p1f0fd6jsn4fc63c166db4",
-          "X-RapidAPI-Host": "currency-exchange.p.rapidapi.com",
+          "X-RapidAPI-Host": "currency-conversion-and-exchange-rates.p.rapidapi.com",
         },
       };
 
       try {
         const response = await fetch(url, options);
-        const result = await response.json();
-        setMoneyList(result);
-        setResultState(result[3]);
-        console.log(result[3]);
+        const result = await response.text();
         console.log(result);
       } catch (error) {
         console.error(error);
