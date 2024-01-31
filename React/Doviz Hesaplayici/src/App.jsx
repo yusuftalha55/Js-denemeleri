3478e38298f8b622739901ccb947564416b95fd2
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-01-31 16:27:15 +0300

    çalışmayan deneme

--- React/Doviz Hesaplayici/src/App.jsx
+++ React/Doviz Hesaplayici/src/App.jsx
@@ -1,25 +1,26 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  
-  
-  
+  const [allMoney, setAllMoney] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const url = "https://currency-conversion-and-exchange-rates.p.rapidapi.com/latest?from=USD&to=EUR%2CGBP";
+      const url =
+        "https://currency-conversion-and-exchange-rates.p.rapidapi.com/latest?from=USD&to=EUR%2CGBP";
       const options = {
         method: "GET",
         headers: {
           "X-RapidAPI-Key":
             "2dab30615cmshc5b0542dbd9da36p1f0fd6jsn4fc63c166db4",
-          "X-RapidAPI-Host": "currency-conversion-and-exchange-rates.p.rapidapi.com",
+          "X-RapidAPI-Host":
+            "currency-conversion-and-exchange-rates.p.rapidapi.com",
         },
       };
 
       try {
         const response = await fetch(url, options);
-        const result = await response.text();
+        const result = await response.json(); 
+        setAllMoney(result);
         console.log(result);
       } catch (error) {
         console.error(error);
@@ -31,7 +32,7 @@ function App() {
 
   return (
     <div>
-      {moneyList.map((money, index) => (
+      {allMoney.map((money, index) => (
         <p key={index}>{money}</p>
       ))}
     </div>
