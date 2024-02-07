ef7043bebebf7826b03f116f033302195920c780
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-02-07 19:07:14 +0300

    api yı ekledim

--- React/Akaryakıt Fiyatları Api/Akaryakit Fiyatlari Api/src/App.jsx
+++ React/Akaryakıt Fiyatları Api/Akaryakit Fiyatlari Api/src/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  
+  useEffect(() => {
+    fetch("https://api.collectapi.com/gasPrice/stateUsaPrice?state=WA", {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+        authorization: "apikey your_token",
+      },
+      credentials: "include",
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error("Error:", error));
+  }, []);
 
-  return (
-    <>
-      
-    </>
-  )
+  return <></>;
 }
 
-export default App
+export default App;
