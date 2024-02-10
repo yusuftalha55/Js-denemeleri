91cbff90149f0a24cc9c1f14b0e8d73581ad95c2
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-02-10 15:11:33 +0300

    gereksiz bir useState kaldırdım

--- React/Akaryakıt Fiyatları Api/Akaryakit Fiyatlari Api/src/App.jsx
+++ React/Akaryakıt Fiyatları Api/Akaryakit Fiyatlari Api/src/App.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [images, setImages] = useState([]);
   const [firstTenimage, setFirstTenimage] = useState([]);
 
   useEffect(() => {
