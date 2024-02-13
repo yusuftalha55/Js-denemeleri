d2119025f44bdadcc94d488e888dd5233891a0aa
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-02-13 16:28:40 +0300

    Ön yüz bitti

--- React/City Quiz/City Quiz/src/App.tsx
+++ React/City Quiz/City Quiz/src/App.tsx
@@ -1,14 +1,30 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  
-
   return (
     <div>
-
-    </div>    
-  )
+      <div className="title">
+        <h1>City Quiz</h1>
+      </div>
+      <div className="question">
+        <p>
+          In which city is there a billboard that turns air into drinkable
+          water?
+        </p>
+      </div>
+      <div className="inputArea">
+        <textarea
+          value={answer}
+          onChange={handleTextareaChange}
+          disabled={status === "submitting"}
+        />
+      </div>
+      <div className="submitButton">
+        <button style={{ margin: 15 }}>Submit</button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
