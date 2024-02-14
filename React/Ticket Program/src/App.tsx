a1314ff9a5a63be75290272d7d2b8228b8f2b8a2
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-02-14 15:28:58 +0300

    önyüz kısmı bitti

--- React/Ticket Program/src/App.tsx
+++ React/Ticket Program/src/App.tsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  
-
   return (
-   <div>
-    
-   </div>
-  )
+    <div>
+      <div className="title">
+        <h1>Let’s check you in</h1>
+      </div>
+      <div className="inputArea">
+        <div>
+          <p className="pClass">Fİrst Name:</p>
+          <input type="text" />
+        </div>
+        <div>
+          <p className="pClass">Last Name:</p>
+          <input className="input" type="text" />
+        </div>
+      </div>
+      <div className="resultArea">
+        <p>Your ticket will be issued to: {}</p>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
