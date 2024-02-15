7f0842104a208dc684cbd0a4ef44995d72501551
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-02-15 19:12:38 +0300

    React deve uygun yazıyorum projeyi

--- React/City Quiz Doc/src/App.tsx
+++ React/City Quiz Doc/src/App.tsx
@@ -1,4 +1,35 @@
 import { useState } from 'react'
 import './App.css'
 
+export default function Form () {
+  const [answer, setAnswer] = useState("");
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState("typing");
 
+  if(status === "success") {
+    return <h1>That's right</h1>
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setStatus("submitting");
+    try {
+      await submitForm(answer);
+      setStatus("success")
+    } catch (err) {
+      setStatus("typing");
+      setError(err);
+    }
+  }
+
+  function handleTextAreaChange(e) {
+    setAnswer(e.target.value);
+  }
+
+  return (
+    <div>
+      <h2>City Quiz</h2>
+
+    </div>
+  )
+}
