6548d062bf01a4dbe7628f6672bc1002b7205baa
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-02-15 19:14:40 +0300

    biraz daha ilerleme

--- React/City Quiz Doc/src/App.tsx
+++ React/City Quiz Doc/src/App.tsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-export default function Form () {
+export default function Form() {
   const [answer, setAnswer] = useState("");
   const [error, setError] = useState(null);
   const [status, setStatus] = useState("typing");
 
-  if(status === "success") {
-    return <h1>That's right</h1>
+  if (status === "success") {
+    return <h1>That's right</h1>;
   }
 
   async function handleSubmit(e) {
@@ -15,7 +15,7 @@ export default function Form () {
     setStatus("submitting");
     try {
       await submitForm(answer);
-      setStatus("success")
+      setStatus("success");
     } catch (err) {
       setStatus("typing");
       setError(err);
@@ -29,7 +29,16 @@ export default function Form () {
   return (
     <div>
       <h2>City Quiz</h2>
-
+      <p>
+        In which city is there a billboard that turns air into drinkable water?
+      </p>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          value={answer}
+          onChange={handleTextareaChange}
+          disabled={status === "submitting"}
+        />
+      </form>
     </div>
-  )
+  );
 }
