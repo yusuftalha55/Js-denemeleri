6542bba77397e4cfc1c7212ccd8347724677dfab
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-02-17 22:09:05 +0300

    İlerleme

--- React/Show Hide Project/src/App.tsx
+++ React/Show Hide Project/src/App.tsx
@@ -1,19 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 export default function Accordion() {
   const [activeIndex, setActiveIndex] = useState(0);
-  return(
+  return (
     <div>
       <h2>Almaty, Kazakhstan</h2>
       <Panel
         title="about"
-        isActive={activeIndex ===0}
-        onShow={()=> setActiveIndex(0)}
+        isActive={activeIndex === 0}
+        onShow={() => setActiveIndex(0)}
       >
-
+        With a population of about 2 million, Almaty is Kazakhstan's largest
+        city. From 1929 to 1997, it was its capital city.
       </Panel>
-
     </div>
-  )
+  );
 }
