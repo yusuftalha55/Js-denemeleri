1b8c69764d4b0dba9118cb767ba638cd915a9d9b
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-02-18 18:50:21 +0300

    ilerleme

--- React/Preserving and resetting state/src/App.tsx
+++ React/Preserving and resetting state/src/App.tsx
@@ -1,10 +1,24 @@
-import { useState } from 'react'
-import Chat from './Chat'
-import ContactList from './ContactList'
-import './App.css'
- 
+import { useState } from "react";
+import Chat from "./Chat";
+import ContactList from "./ContactList";
+import "./App.css";
+
 export default function Messanger() {
   const [to, setTo] = useState(contacts[0]);
+
+  return (
+    <div>
+      <ContactList
+        contacts={contacts}
+        selectedContact={to}
+        onSelect={(contact) => setTo(contact)}
+      />
+      <Chat key={to.email} contact={to} />
+    </div>
+  );
 }
 
+const contacts = [
+   { name: "Taylor" , email: "taylor@mail.com" },
+];
 
