14e091c14490b80326151d5a1ec20379211cb929
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-02-18 18:46:28 +0300

    ilerleme

--- React/Preserving and resetting state/src/App.tsx
+++ React/Preserving and resetting state/src/App.tsx
@@ -1,4 +1,10 @@
 import { useState } from 'react'
+import Chat from './Chat'
+import ContactList from './ContactList'
 import './App.css'
+ 
+export default function Messanger() {
+  const [to, setTo] = useState(contacts[0]);
+}
 
 
