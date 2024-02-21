0f58536f8e216d8d8caa42688e4597bd54794ce9
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-02-21 14:49:04 +0300

    ilerleme

--- React/Prague itinerary/src/TaskList.tsx
+++ React/Prague itinerary/src/TaskList.tsx
@@ -1,21 +1,33 @@
 import { useState } from "react";
 
-export default function TaskList({
-  tasks,
-  onChangeTask,
-  onDleteTask
-}) {
-    return (
-        <ul>
-            {tasks.map(task =>(
-                <li key={task.id}>
-                    <task
-                      task = {task}
-                      onChange = {onChangeTask}
-                      onDelete = {onDeleteTask}
-                    />
-                </li>
-            ))}
-        </ul>
+export default function TaskList({ tasks, onChangeTask, onDleteTask }) {
+  return (
+    <ul>
+      {tasks.map((task) => (
+        <li key={task.id}>
+          <task task={task} onChange={onChangeTask} onDelete={onDeleteTask} />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function Task({ task, onChange, onDelete }) {
+  const [isEditing, setIsEditing] = useState(false);
+  let taskContent;
+  if (isEditing) {
+    taskContent = (
+      <div>
+        <input
+          value={task.text}
+          onChange={(e) => {
+            onChange({
+              ...task,
+              text: e.target.value,
+            });
+          }}
+        />
+      </div>
     );
+  }
 }
