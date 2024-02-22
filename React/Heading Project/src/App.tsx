c5ce3a76130fb3ba859963fd4d696d8de4e18b70
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-02-22 16:00:35 +0300

    Proje bitti

--- React/Heading Project/src/App.tsx
+++ React/Heading Project/src/App.tsx
@@ -1,1 +1,25 @@
+import Heading from "./Heading";
+import Section from "./Section";
 
+export default function Page() {
+    return (
+        <Section>
+            <Heading>Title</Heading>
+            <Section>
+                <Heading>Heading</Heading>
+                <Heading>Heading</Heading>
+                <Heading>Heading</Heading>
+                <Section>
+                   <Heading>Sub-heading</Heading>
+                   <Heading>Sub-heading</Heading>
+                   <Heading>Sub-heading</Heading>
+                   <Section>
+                       <Heading>Sub-heading</Heading> 
+                       <Heading>Sub-heading</Heading>
+                       <Heading>Sub-heading</Heading>
+                   </Section>
+                </Section>
+            </Section>
+        </Section>
+    )
+}
--- React/Heading Project/src/main.tsx
+++ React/Heading Project/src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import Page from './App.tsx'
+import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Page />
   </React.StrictMode>,
 )
