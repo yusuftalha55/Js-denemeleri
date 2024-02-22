f467f23d2e6888d6d2575971ed82c877495c76c9
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-02-22 15:47:49 +0300

    ilerleme

--- React/Heading Project/src/Heading.tsx
+++ React/Heading Project/src/Heading.tsx
@@ -1,12 +1,26 @@
 import { useContext } from "react";
-import {LevelContext} from './LevelContext.tsx';
+import { LevelContext } from "./LevelContext.tsx";
 
-export default function Heading( {children} ) {
-    const level = useContext(LevelContext);
-    switch (level) {
-        case 0:
-            throw Error ("Heading must be inside a section!");
-        case 1:
-            return <h1>{children}</h1>;
-    }
+export default function Heading({ children }) {
+  const level = useContext(LevelContext);
+  switch (level) {
+    case 0:
+      throw Error("Heading must be inside a section!");
+    case 1:
+      return <h1>{children}</h1>;
+    case 2:
+      return <h1>{children}</h1>;
+
+    case 3:
+      return <h1>{children}</h1>;
+
+    case 4:
+      return <h1>{children}</h1>;
+
+    case 5:
+      return <h1>{children}</h1>;
+
+    case 6:
+      return <h1>{children}</h1>;
+  }
 }
