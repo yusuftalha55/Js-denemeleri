613a5eb43f49618a2ba6e07ea5a7654dd9e55832
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-02-23 20:46:19 +0300

    ilerleme

--- React/Global State/src/App.tsx
+++ React/Global State/src/App.tsx
@@ -1,4 +1,12 @@
-
+import AddTask from './AddTask.tsx';
+import TaskList from './TaskList.tsx';
+import { TaskProvider } from './TasksContext.tsx';
 import './App.css'
+
+export default function TaskApp() {
+  return (
+    
+  )
+}
 
 
