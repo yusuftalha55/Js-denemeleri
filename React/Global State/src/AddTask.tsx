1afeb74f2f39ab53ab872d4776c1a337cef577e4
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-02-24 17:06:32 +0300

    AddTask bitti

--- React/Global State/src/AddTask.tsx
+++ React/Global State/src/AddTask.tsx
@@ -5,6 +5,23 @@ export default function AddTask({ onAddTask }) {
     const [text, setText] = useState("");
     const dispatch = useTasksDispatch();
     return (
-
+        <div>
+           <input 
+              placeholder="Add Task"
+              value={text}
+              onChange={e => setText(e.target.value)}
+           />
+           <button onClick={() => {
+              setText("");
+              dispatch({
+                type: "added",
+                id: nextId++,
+                text: text,
+              });
+           }}>
+            Add
+           </button>
+        </div>
     );
 }
+let nextId = 3;
