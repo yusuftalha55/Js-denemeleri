c2eabdb181df7f238e1388588b983a50cc871b36
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-02-24 17:11:11 +0300

    ilerleme

--- React/Global State/src/TaskList.tsx
+++ React/Global State/src/TaskList.tsx
@@ -1,2 +1,15 @@
 import { useState, useContext } from "react";
 import {useTasks, useTasksDispatch } from './TasksContext.tsx';
+
+export default function TaskList() {
+    const tasks = useTasks();
+    return (
+        <ul>
+            {tasks.map(task =>(
+                <li key={task.id}>
+                    <task task= {task}/>
+                </li>
+            ))}
+        </ul>
+    );
+}
