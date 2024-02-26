f95c4d1204e08dfc402a1e534ee52e8b4d74ce46
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-02-26 18:45:43 +0300

    ilerleme

--- React/Global State/src/TasksContext.tsx
+++ React/Global State/src/TasksContext.tsx
@@ -2,3 +2,10 @@ import { createContext, useContext, useReducer } from "react";
 
 const TasksContext = createContext(null);
 const TasksDispatchContext = createContext(null);
+
+export default function TasksProvider({ children }) {
+    const [tasks, dispatch] = useReducer(
+        tasksReducer,
+        initialTasks
+    );
+}
