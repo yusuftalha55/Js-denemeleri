04fb79fc5f1544a099f0aa4f69f5aebfea092053
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-02-28 21:46:21 +0300

    Çalışan düzgün hali

--- My Project/SportTip/src/App.tsx
+++ My Project/SportTip/src/App.tsx
@@ -3,15 +3,12 @@ import './App.css'
 import DataPage from './data'
 
 function App() {
+
   return (
     <div className='main'>
-      {/* Örnek bir title ve description props'u ile DataPage bileşenini çağırın */}
-      <DataPage 
-        title="Antrenman" 
-        description="Spor yaparken, doğaya çıkın ve açık havada egzersiz yapın. Doğa yürüyüşleri, bisiklet sürme veya dağ tırmanışı gibi aktiviteler size zihinsel ve fiziksel olarak fayda sağlar." 
-      />
+      <DataPage></DataPage>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
