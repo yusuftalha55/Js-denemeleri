9d1dbf773b7b3d4163c019259b01be74f2a7045a
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-03-01 17:53:56 +0300

    denemeler

--- My Project/SportTip/src/DarkModeToggleButton.tsx
+++ My Project/SportTip/src/DarkModeToggleButton.tsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 
 
 export default function ToggleButton = () {
-  
+  const [backGroundColor, setBackGroundColor] = useContext("#a9a9a9");
+
+  return (
+    <div>
+      <button>{ChangeButtonName}</button>
+    </div>
+  )
 }
