b35797838c5784c0478b0703719dd105e7b5a4ae
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-03-01 17:55:14 +0300

    denemeler

--- My Project/SportTip/src/DarkModeToggleButton.tsx
+++ My Project/SportTip/src/DarkModeToggleButton.tsx
@@ -3,10 +3,13 @@ import React, { useContext, useState } from 'react';
 
 export default function ToggleButton = () {
   const [backGroundColor, setBackGroundColor] = useContext("#a9a9a9");
+  const [ChangeButtonName, setChangeButtonName] = ("Karanlıık Mod")
 
   return (
     <div>
-      <button>{ChangeButtonName}</button>
+      <button>
+        {ChangeButtonName}
+      </button>
     </div>
   )
 }
