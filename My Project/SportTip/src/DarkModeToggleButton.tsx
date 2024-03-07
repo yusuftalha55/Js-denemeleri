5f5a8766f39858eee7270b7a0a445d0248b75a3d
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-03-07 17:17:02 +0300

    fazlalık kısımlar atıldı

--- My Project/SportTip/src/DarkModeToggleButton.tsx
+++ My Project/SportTip/src/DarkModeToggleButton.tsx
@@ -2,30 +2,7 @@ import React, { useState, createContext, useContext } from 'react';
 
 export const ChangeBackGroundColor = createContext({});
 
-// export default backGroundContext;
 
-const ToggleButton: React.FC = () => {
-  const [backGroundColor, setBackGroundColor] = useState("#faebd7");
-  const [ChangeButtonName, setChangeButtonName] = useState("Aydınlık Mod");
-  const [textColor ,setTextColor] = useState("black");
-
-  function ChangeBackgroundFunction() {
-    setChangeButtonName(ChangeButtonName === "Karanlık Mod" ? "Aydınlık Mod" : "Karanlık Mod");
-    setBackGroundColor(backGroundColor === "#faebd7" ? "#404040" : "#faebd7");
-    setTextColor(textColor === "black" ? "white" : "black" );
-  }
-
-  return (
-    <div>
-      <button onClick={ChangeBackgroundFunction} 
-              style={{ backgroundColor: backGroundColor, color: textColor, height:50, borderRadius:15, boxShadow: "5px 3px 15px rgba(209, 1, 1, 0.5)"}}>
-        {ChangeButtonName}
-      </button>
-    </div>
-  );
-}
-
-export default ToggleButton;
 
 
 
