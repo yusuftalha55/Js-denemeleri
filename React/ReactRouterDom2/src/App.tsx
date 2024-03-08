632d895c5ca5efe693b75b08410163bfcdab9e87
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-03-08 18:25:47 +0300

    ilerleme

--- React/ReactRouterDom2/src/App.tsx
+++ React/ReactRouterDom2/src/App.tsx
@@ -1,19 +1,24 @@
-import { Routes, Route, Link, NavLink } from "react-router-dom"
-import Home from "./pages/Home"
-import Contact from "./pages/Contact"
-import Blog from "./pages/Blog"
-
+import { Routes, Route, Link, NavLink } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Blog from "./pages/Blog";
 
 function App() {
-  
-
   return (
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/contact" element={<Contact/>}/>
-      <Route path="/blog" element={<Blog/>}/>
-    </Routes>
-  )
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/blog">Blog</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
