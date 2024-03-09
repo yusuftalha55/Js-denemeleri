1c42a4cc0ff10a362be6d3b3b991adc348e1b5bb
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-03-09 17:36:43 +0300

    Root.tsx i ana sayfaya ekledim

--- My Project/SportTip/src/App.tsx
+++ My Project/SportTip/src/App.tsx
@@ -1,30 +1,56 @@
 import { useEffect, useState } from "react";
-import './App.css';
-import DataPage from './data';
-import { ChangeBackGroundColor } from './DarkModeToggleButton';
+import { Routes, Route, Link, NavLink } from "react-router-dom";
+import "./App.css";
+import DataPage from "./data";
+import { ChangeBackGroundColor } from "./DarkModeToggleButton";
+import Root from "./pages/Root";
 
 function App() {
   const [backGroundColor, setBackGroundColor] = useState("#faebd7");
   const [ChangeButtonName, setChangeButtonName] = useState("Aydınlık Mod");
-  const [textColor ,setTextColor] = useState("black");
+  const [textColor, setTextColor] = useState("black");
 
   useEffect(() => {
-    document.body.style.setProperty('--background-color', backGroundColor);
+    document.body.style.setProperty("--background-color", backGroundColor);
   }, [backGroundColor]);
 
   function changeButton() {
-    setChangeButtonName(ChangeButtonName === "Karanlık Mod" ? "Aydınlık Mod" : "Karanlık Mod");
+    setChangeButtonName(
+      ChangeButtonName === "Karanlık Mod" ? "Aydınlık Mod" : "Karanlık Mod"
+    );
     setBackGroundColor(backGroundColor === "#faebd7" ? "#404040" : "#faebd7");
-    setTextColor(textColor === "black" ? "white" : "black" );
+    setTextColor(textColor === "black" ? "white" : "black");
   }
   return (
     <ChangeBackGroundColor.Provider
       value={{ backGroundColor, setBackGroundColor }}
     >
-      <div className='main' style={{ backgroundColor: backGroundColor }}>
-        <DataPage/>
+      <div className="navbar">
+        <nav>
+          <NavLink to="/root">Kaydedilen Öneriler</NavLink>
+        </nav>
+
+        <Routes>
+          <Route path="/root" element={<Root />} />
+        </Routes>
+      </div>
+      <div className="main" style={{ backgroundColor: backGroundColor }}>
+        <DataPage />
         <div>
-          <button onClick={changeButton}  style={{ backgroundColor: backGroundColor, color: textColor, height:50, borderRadius:15, boxShadow: "5px 3px 15px rgba(209, 1, 1, 0.5)", fontSize:15, fontWeight:"bold" }}>Toggle Dark Mode</button>
+          <button
+            onClick={changeButton}
+            style={{
+              backgroundColor: backGroundColor,
+              color: textColor,
+              height: 50,
+              borderRadius: 15,
+              boxShadow: "5px 3px 15px rgba(209, 1, 1, 0.5)",
+              fontSize: 15,
+              fontWeight: "bold",
+            }}
+          >
+            Toggle Dark Mode
+          </button>
         </div>
       </div>
     </ChangeBackGroundColor.Provider>
