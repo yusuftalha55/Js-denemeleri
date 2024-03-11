942b725377177b5660f4423b3b1552dcea71ff0f
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-03-11 15:24:18 +0300

    ilerleme

--- React/ReactRouterDom2/src/pages/Home.tsx
+++ React/ReactRouterDom2/src/pages/Home.tsx
@@ -1,7 +1,10 @@
-import React from "react"
+import React, { useContext } from "react"
 import { ChangeBackGroundColor } from "../globalState"
 
 export default function Home() {
+    
+    const {BackGroundColor, setBackGroundColor} = useContext(ChangeBackGroundColor);
+
     return (
         <div>
             Home Page
