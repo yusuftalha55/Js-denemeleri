696d21011080c7a46ca49f17cebf0f8507cebf13
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-03-11 15:21:36 +0300

    İlerleme

--- React/ReactRouterDom2/src/App.tsx
+++ React/ReactRouterDom2/src/App.tsx
@@ -2,10 +2,15 @@ import { Routes, Route, Link, NavLink } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Blog from "./pages/Blog";
+import { ChangeBackGroundColor } from "./globalState";
+import { useState } from "react";
 
 function App() {
+
+  const [BackGroundColor, setBackGroundColor] = useState("#faebd7");
+
   return (
-    <div>
+    <ChangeBackGroundColor.Provider value={{ BackGroundColor, setBackGroundColor }}>
       <nav>
         <NavLink to="/">Home</NavLink>
         <NavLink to="/contact">Contact</NavLink>
@@ -17,7 +22,7 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/blog" element={<Blog />} />
       </Routes>
-    </div>
+    </ChangeBackGroundColor.Provider>
   );
 }
 
