64b18a79afe691568db7179950c7631cbb600c5f
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-03-11 15:26:34 +0300

    ilerleme

--- React/ReactRouterDom2/src/pages/Home.tsx
+++ React/ReactRouterDom2/src/pages/Home.tsx
@@ -6,8 +6,13 @@ export default function Home() {
     const {BackGroundColor, setBackGroundColor} = useContext(ChangeBackGroundColor);
 
     return (
-        <div>
-            Home Page
+        <div style={{backgroundColor:BackGroundColor}}>
+           <div className="title">
+             <h1>Home Page</h1>
+           </div>
+           <div className="active">
+            <button>Change Background Color</button>
+           </div>
         </div>
     )
 }
