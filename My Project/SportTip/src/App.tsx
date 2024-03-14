03084a1898216995a1f667116ca08798aa4656f6
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-03-14 16:47:27 +0300

    dosya isimlendirmesinde düzeltme

--- My Project/SportTip/src/App.tsx
+++ My Project/SportTip/src/App.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Routes, Route, NavLink } from "react-router-dom";
 import "./App.css";
 import DataPage from "./data";
-import Root from "./pages/Root";
+import Root from "./pages/SavedSuggestions";
 import { ChangeBackGroundColor } from "./DarkModeToggleButton";
 
 function App() {
