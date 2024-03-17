d27ebd8bc2e2da752fbab7aa801cbd41e128cf29
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-03-17 16:19:52 +0300

    SavedInformations contextini app.tsx e ekledim

--- My Project/SportTip/src/App.tsx
+++ My Project/SportTip/src/App.tsx
@@ -4,27 +4,30 @@ import "./App.css";
 import DataPage from "./data";
 import Root from "./pages/SavedSuggestions";
 import { ChangeBackGroundColor } from "./DarkModeToggleButton";
+import { SavedInformations } from "./DarkModeToggleButton";
 
 function App() {
   const [BackGroundColor, setBackGroundColor] = useState("#faebd7");
+  const [savedCurrentData, setSavedCurrentData] = useState();
 
   return (
     <ChangeBackGroundColor.Provider
       value={{ BackGroundColor, setBackGroundColor }}
     >
-      <nav>
-        <NavLink className="navlinkHome" to="/">
-          Ana Sayfa
-        </NavLink>
-        <NavLink className="navlinkContact" to="/save">
-          Kaydedilenler
-        </NavLink>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<DataPage />} />
-        <Route path="/save" element={<Root />} />
-      </Routes>
+      <SavedInformations.Provider value={savedCurrentData, setSavedCurrentData}>
+        <nav>
+          <NavLink className="navlinkHome" to="/">
+            Ana Sayfa
+          </NavLink>
+          <NavLink className="navlinkContact" to="/save">
+            Kaydedilenler
+          </NavLink>
+        </nav>
+        <Routes>
+          <Route path="/" element={<DataPage />} />
+          <Route path="/save" element={<Root />} />
+        </Routes>
+      </SavedInformations.Provider>
     </ChangeBackGroundColor.Provider>
   );
 }
