c1274d558dbc6a6a6f57993918b2c19b0c09d718
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-03-24 02:26:32 +0300

    button eklendi

--- My Project/SportTip/src/pages/SavedSuggestions.tsx
+++ My Project/SportTip/src/pages/SavedSuggestions.tsx
@@ -1,14 +1,16 @@
 import React, { useContext, useState } from "react";
-import { ChangeBackGroundColor, SavedInformations } from "../DarkModeToggleButton";
+import {
+  ChangeBackGroundColor,
+  SavedInformations,
+} from "../DarkModeToggleButton";
 import { DataItem } from "../data";
 import { MainSavedData } from "../DarkModeToggleButton";
 
-
 const Root: React.FC<{ data: DataItem }> = (props) => {
-  
-  const { BackGroundColor, setBackGroundColor } = useContext(ChangeBackGroundColor);
-  const { savedData, setSavedData} = useContext(MainSavedData);
-  
+  const { BackGroundColor, setBackGroundColor } = useContext(
+    ChangeBackGroundColor
+  );
+  const { savedData, setSavedData } = useContext(MainSavedData);
 
   function changeButton() {
     const newColor = BackGroundColor === "#faebd7" ? "#404040" : "#faebd7";
@@ -16,21 +18,23 @@ const Root: React.FC<{ data: DataItem }> = (props) => {
     setBackGroundColor(newColor);
   }
 
- 
-
   return (
     <div style={{ backgroundColor: BackGroundColor }}>
-      
       <div>
         <div className="containerSaved">
-        <div>
-          {savedData.map((item, index) => (
-            <p key={index}>
-              <h3>{item.id}: {item.title}</h3>
-              <p>{item.description}</p>
-            </p>
-          ))}
-        </div>
+          <div>
+            {savedData.map((item, index) => (
+              <p key={index}>
+                <h3>
+                  {item.id}: {item.title}
+                </h3>
+                <p>{item.description}</p>
+              </p>
+            ))}
+          </div>
+          <div className="deleteButton">
+            <button>Kaydedileni sil</button>
+          </div>
         </div>
       </div>
       <div className="active">
@@ -42,7 +46,7 @@ const Root: React.FC<{ data: DataItem }> = (props) => {
             boxShadow: "5px 3px 15px rgba(209, 1, 1, 0.5)",
             fontSize: 15,
             fontWeight: "bold",
-            marginLeft:15,
+            marginLeft: 15,
           }}
         >
           Change Background Color
@@ -50,6 +54,6 @@ const Root: React.FC<{ data: DataItem }> = (props) => {
       </div>
     </div>
   );
-}
+};
 
 export default Root;
