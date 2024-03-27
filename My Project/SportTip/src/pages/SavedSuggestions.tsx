4e41a17cf9c6e2b2d36ebbf1721f3accf8d64061
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-03-27 05:33:05 +0300

    css düzeltmeleri

--- My Project/SportTip/src/pages/SavedSuggestions.tsx
+++ My Project/SportTip/src/pages/SavedSuggestions.tsx
@@ -35,20 +35,20 @@ const Root: React.FC<{ data: DataItem }> = (props) => {
                 {item.id}: {item.title}
               </h3>
               <p>{item.description}</p>
-              <div className="deleteButton">
-                <button onClick={() => DeleteToSavedData(index)}
-                  style={{
-                    color: "#18c208",
-                    cursor: "pointer",
-                    backgroundColor: "rgb(215, 250, 238)",
-                    fontWeight: "bold",
-                    boxShadow: "5px 3px 15px rgba(209, 1, 1, 0.5)",
-                    fontSize: "medium",
-                    borderRadius: 15,
-                    transition: "color 0.3s ease",
-                    width: 120,
-                    height: 50,
-                  }}
+              <div className="deleteButtonMain">
+                <button className="deleteButton" onClick={() => DeleteToSavedData(index)}
+                  // style={{
+                  //   color: "#18c208",
+                  //   cursor: "pointer",
+                  //   backgroundColor: "rgb(215, 250, 238)",
+                  //   fontWeight: "bold",
+                  //   boxShadow: "5px 3px 15px rgba(209, 1, 1, 0.5)",
+                  //   fontSize: "medium",
+                  //   borderRadius: 15,
+                  //   transition: "color 0.3s ease",
+                  //   width: 120,
+                  //   height: 50,
+                  // }}
                 >
                   Kaydedileni sil
                 </button>
