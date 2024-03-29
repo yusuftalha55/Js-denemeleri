5ccbb873187452f066458cad3e98d8c812142d39
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-03-29 21:37:22 +0300

    İnputları ekledim

--- My Project/SportTip/src/BodyMassIndex.tsx
+++ My Project/SportTip/src/BodyMassIndex.tsx
@@ -4,8 +4,12 @@ import "./App.css";
 const BodyIndex = () => {
     return(
         <div className="bodyIndexclass">
+            <h1>Vücut Kitle Endeksi</h1>
             <div className="inputBodyClass">
-            
+                <h3>Boyunuzu giriniz</h3>
+                <input type="number" min={0}/>
+                <h3>Kilonuzu giriniz</h3>
+                <input type="number" min={0}/>
             </div>
         </div>
     )
