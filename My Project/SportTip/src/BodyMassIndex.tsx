a65fd0517b051e194a8beb7048d1f86777dd9848
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-03-29 21:45:37 +0300

    ilerleme

--- My Project/SportTip/src/BodyMassIndex.tsx
+++ My Project/SportTip/src/BodyMassIndex.tsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 const BodyIndex = () => {
+    const [weight, setWeight] = useState(0);
+    const [height, setHeight] = useState(0);
+    const [bodyIndex, setBodyIndex] = useState(0);
+
+
+
+    function mathButton () {
+        
+
+    }
     return(
         <div className="bodyIndexclass">
             <h1>Vücut Kitle Endeksi</h1>
             <div className="inputBodyClass">
                 <h3>Boyunuzu giriniz</h3>
-                <input type="number" min={0}/>
+                <input type="number" min={0} value={height} onChange={(e)=> setHeight(e.target.value)}/>
                 <h3>Kilonuzu giriniz</h3>
-                <input type="number" min={0}/>
+                <input type="number" min={0} value={weight} onChange={(e)=> setWeight(e.target.value)}/>
+            </div>
+            <button onClick={mathButton}>Hesapla</button>
+            <div className="output">
+                <h3>Vücut kitle endeksiniz: {bodyIndex}</h3>
             </div>
         </div>
     )
