d094bbea6dbae995a1d978ecda2b36fd2770d5ab
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-04-03 23:19:33 +0300

    Projeye nekt.js kurdum

--- React/ReactRouterDom2/src/App.tsx
+++ React/ReactRouterDom2/src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route, Link, NavLink } from "react-router-dom";
-import Home from "./pages/Home";
-import Contact from "./pages/Contact";
-import Blog from "./pages/Blog";
+// import Home from "./pages/Home";
+// import Contact from "./pages/Contact";
+// import Blog from "./pages/Blog";
 import { ChangeBackGroundColor } from "./globalState";
 import { useState } from "react";
 
@@ -11,7 +11,13 @@ function App() {
 
   return (
     <ChangeBackGroundColor.Provider value={{ BackGroundColor, setBackGroundColor }}>
-      <nav>
+      <div>
+
+      </div>
+      
+      
+      
+      {/* <nav>
         <NavLink to="/">Home</NavLink>
         <NavLink to="/contact">Contact</NavLink>
         <NavLink to="/blog">Blog</NavLink>
@@ -21,7 +27,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/blog" element={<Blog />} />
-      </Routes>
+      </Routes> */}
     </ChangeBackGroundColor.Provider>
   );
 }
