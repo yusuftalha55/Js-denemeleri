3b107859d210c832c383745d896d2600015b3521
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-04-04 11:17:51 +0300

    Nextjs proje kurulumu tamamlandı

--- React/ReactRouterDom2/src/App.tsx
+++ React/ReactRouterDom2/src/App.tsx
@@ -1,22 +1,25 @@
-import { Routes, Route, Link, NavLink } from "react-router-dom";
+// import { Routes, Route, NavLink } from "react-router-dom";
 // import Home from "./pages/Home";
 // import Contact from "./pages/Contact";
 // import Blog from "./pages/Blog";
 import { ChangeBackGroundColor } from "./globalState";
 import { useState } from "react";
+import Link from "next/link";
 
 function App() {
-
   const [BackGroundColor, setBackGroundColor] = useState("#faebd7");
 
   return (
-    <ChangeBackGroundColor.Provider value={{ BackGroundColor, setBackGroundColor }}>
-      <div>
-
+    <ChangeBackGroundColor.Provider
+      value={{ BackGroundColor, setBackGroundColor }}
+    >
+      <div style={{ backgroundColor }}>
+        <h1>Sayfa adları</h1>
+        <Link href="/">Home</Link>
+        <Link href="/contact">Contact</Link>
+        <Link href="/blog">Blog</Link>
       </div>
-      
-      
-      
+
       {/* <nav>
         <NavLink to="/">Home</NavLink>
         <NavLink to="/contact">Contact</NavLink>
