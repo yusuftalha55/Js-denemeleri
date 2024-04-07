73e9ef07df75d35634a0d03cf5e89bfa6070ac4f
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-04-07 18:35:33 +0300

    Siteye yeni eklemeler yaptım

--- React/nextjsapp/app/page.tsx
+++ React/nextjsapp/app/page.tsx
@@ -6,15 +6,20 @@ export default function Home() {
   return (
     <ul>
       <li>
-        <Link href="/">Home</Link>
+        <h2>
+          <Link href="/">Home</Link>
+        </h2>
       </li>
       <li>
-        <Link href="/pages">About Us</Link>
+        <h2>
+          <Link href="/pages">About Us</Link>
+        </h2>
       </li>
       <li>
-        <Link href="/blog/hello-world">Blog Post</Link>
+        <h2>
+          <Link href="/blog/hello-world">Blog Post</Link>
+        </h2>
       </li>
     </ul>
   );
 }
-
--- React/nextjsapp/app/pages/page.tsx
+++ React/nextjsapp/app/pages/page.tsx
@@ -1,6 +1,12 @@
+import React from "react";
+import Link from "next/link";
+
 export default function About () {
     return (
         <main>
+            <h1>
+                <Link href="/">Ana Sayfa</Link>
+            </h1>
             <h2>News</h2>
         </main>
     );
