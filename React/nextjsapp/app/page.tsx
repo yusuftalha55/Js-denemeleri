6ca05c5626bbf12d9e564250ceac147535d9162e
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-04-10 15:36:37 +0300

    Sitenin son hali

--- React/nextjsapp/app/page.tsx
+++ React/nextjsapp/app/page.tsx
@@ -1,8 +1,15 @@
+// @ts-nocheck
 import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
+import { ChangeBackGroundColor } from "./globalState";
+import React, {useState, useEffect} from "react";
 
 export default function Home() {
+  // const [backGroundColor, setBackGroundColor] = useState("#D1DBBD");
+
+
+
   return (
     <ul>
       <li>
