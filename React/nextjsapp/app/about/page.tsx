aef9939995ff70b0734001e8ec645b08c26605f4
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-04-15 16:47:14 +0300

    ufak düzenleme ve eklemeler

--- React/nextjsapp/app/about/page.tsx
+++ React/nextjsapp/app/about/page.tsx
@@ -3,21 +3,20 @@ import React, { useContext } from "react";
 import Link from "next/link";
 import { ChangeBackGroundColor } from "../globalState";
 
-export default function About () {
-    const { BackGroundColor, setBackGroundColor} = useContext( ChangeBackGroundColor);
-    return (
-        <main style={{ backgroundColor:BackGroundColor}}>
-            <h1>
-                <Link href="/">Ana Sayfa</Link>
-                
-            </h1>
-            
-            <h2>
-                About
-            </h2>
-            <h2>
-                <Link href="about/details">Detaylar</Link>
-            </h2>
-        </main>
-    );
+export default function About() {
+  const { BackGroundColor, setBackGroundColor } = useContext(
+    ChangeBackGroundColor
+  );
+  return (
+    <main style={{ backgroundColor: BackGroundColor }}>
+      <h1>
+        <Link href="/">Ana Sayfa</Link>
+      </h1>
+
+      <h2>About</h2>
+      <h2>
+        <Link href="about/details">Detaylar</Link>
+      </h2>
+    </main>
+  );
 }
