a1fc21e6d7362e1756fe6f959838bd8bf5ddaca0
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-04-19 16:13:32 +0300

    logo eklemeyi başardım

--- twitter-clone/app/page.tsx
+++ twitter-clone/app/page.tsx
@@ -3,13 +3,14 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
 import { useState } from "react";
+import xLogo from "../app/public/img/xLogo.png";
 
 export default function Home() {
   return (
     <div className="main">
       <div className="sideBar">
         <Link href="/pages/homePage">
-        <img src="/img/xLogo.png" alt="Ana Sayfa" />
+        <Image src={xLogo} alt="Ana Sayfa" />
         </Link>
       </div>
 
