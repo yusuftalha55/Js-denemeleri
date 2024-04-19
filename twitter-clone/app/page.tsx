2d835fa42a5a69dbd67f99f2f4589ca686183202
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-04-19 16:22:02 +0300

    ana sayfa logosu da eklendi

--- twitter-clone/app/page.tsx
+++ twitter-clone/app/page.tsx
@@ -4,14 +4,22 @@ import styles from "./page.module.css";
 import Link from "next/link";
 import { useState } from "react";
 import xLogo from "../app/public/img/xLogo.png";
+import homePage from "../app/public/img/homePage.png";
 
 export default function Home() {
   return (
     <div className="main">
       <div className="sideBar">
-        <Link href="/pages/homePage">
-          <Image src={xLogo} width={30} height={30} alt="Ana Sayfa" />
-        </Link>
+        <div className="sideBarHomePage">
+          <Link href="/pages/homePage">
+            <Image src={xLogo} width={30} height={25} alt="Ana Sayfa" />
+          </Link>
+        </div>
+        <div className="sideBarHomePage">
+          <Link href="/pages/homePage">
+            <Image src={homePage} width={30} height={25} alt="Ana Sayfa" />
+          </Link>
+        </div>
       </div>
     </div>
   );
