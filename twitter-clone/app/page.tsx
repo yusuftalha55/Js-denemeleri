951ffe7c9527fc46b1d1c3a9a954e613db45c523
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-04-19 15:44:23 +0300

    ufak ekleme ve düzeltmeler

--- twitter-clone/app/page.tsx
+++ twitter-clone/app/page.tsx
@@ -1,5 +1,8 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
+import { useState } from "react";
 
 export default function Home() {
   return (
