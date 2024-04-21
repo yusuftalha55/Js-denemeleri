0a52a1d5c44482644b16502b28b6a0606f619a6c
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-04-21 14:41:47 +0300

    lists page tamamlandı

--- twitter-clone/app/page.tsx
+++ twitter-clone/app/page.tsx
@@ -8,6 +8,7 @@ import homePage from "../app/public/img/homePage.png";
 import searchButton from "../app/public/img/searchButton.png";
 import notificationButton from "../app/public/img/notification.png";
 import messageButton from "../app/public/img/message.png";
+import ListsButton from "../app/public/img/lists.png";
 
 export default function Home() {
   return (
@@ -36,6 +37,11 @@ export default function Home() {
         <div className="sideBarMessagesPage">
           <Link href="/pages/messagesPage">
             <Image src={messageButton} width={30} height={25} alt="Ara" />
+          </Link>
+        </div>
+        <div className="sideBarListsPage">
+          <Link href="/pages/listsPage">
+            <Image src={ListsButton} width={30} height={25} alt="Ara" />
           </Link>
         </div>
       </div>
