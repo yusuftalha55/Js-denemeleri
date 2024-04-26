769a060b7a0d564853645d9afa25081d82b2188c
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-04-26 15:36:51 +0300

    simgelere tıklayınca sayfa içinde render etmeyi başardım

--- twitter-clone/app/page.tsx
+++ twitter-clone/app/page.tsx
@@ -1,9 +1,8 @@
 "use client";
 import Image from "next/image";
-import styles from "./page.module.css";
 import Link from "next/link";
 import { useState } from "react";
-import "./globals.css";
+import styles from "./page.module.css"; // Modül stilleri burada kullanılabilir.
 import xLogo from "../app/public/img/xLogo.png";
 import homePage from "../app/public/img/homePage.png";
 import searchButton from "../app/public/img/searchButton.png";
@@ -15,9 +14,8 @@ import groupsButton from "../app/public/img/groups.png";
 import profilesButton from "../app/public/img/profiles.png";
 import moreButton from "../app/public/img/more.png";
 import sendButton from "../app/public/img/send.png";
-import MainPage from "./pages/homePage/page";
-import SearchButtonPage from "./pages/searchButtonPage/page";
-
+import MainPage from "./pages/homePage/page"; // Ana sayfa bileşeni burada import edilmiş.
+import SearchButtonPage from "./pages/searchButtonPage/page"; // Arama sayfası bileşeni burada import edilmiş.
 
 export default function Home() {
   const [activePage, setActivePage] = useState("home");
@@ -26,78 +24,68 @@ export default function Home() {
     setActivePage(page);
   };
 
-
   return (
-    <div className="main">
-      <div className="sideBar">
-        <div className="sideBarHomePage">
-          <Link href="/pages/homePage">
-            <Image src={xLogo} width={30} height={25} alt="Ana Sayfa" onClick={() => handlePageChange("home")}/>
-          </Link>
+    <div className={styles.container}>
+      <div className={styles.sideBar}>
+        <div className={styles.sideBarHomePage}>
+          <button onClick={() => handlePageChange("home")}>
+            <Image src={xLogo} width={30} height={25} alt="Ana Sayfa" />
+          </button>
         </div>
-        <div className="sideBarHomePage">
-          <Link href="/pages/homePage">
-            <Image src={homePage} width={30} height={25} alt="Ana Sayfa" onClick={() => handlePageChange("home")} />
-          </Link>
+        <div className={styles.sideBarHomePage}>
+          <button onClick={() => handlePageChange("home")}>
+            <Image src={homePage} width={30} height={25} alt="Ana Sayfa" />
+          </button>
         </div>
-        <div className="sideBarSearchButtonPage">
-          <Link href="/pages/searchButtonPage">
-            <Image src={searchButton} width={30} height={25} alt="Ara" onClick={() => handlePageChange("searchButton")} />
-          </Link>
+        <div className={styles.sideBarSearchButtonPage}>
+          <button onClick={() => handlePageChange("search")}>
+            <Image src={searchButton} width={30} height={25} alt="Ara" />
+          </button>
         </div>
-        <div className="sideBarNotificationPage">
+        <div className={styles.sideBarNotificationPage}>
           <Link href="/pages/notificationPage">
-            <Image src={notificationButton} width={30} height={25} alt="Ara" onClick={() => handlePageChange("searchButton")} />
+            <Image src={notificationButton} width={30} height={25} alt="Bildirimler" />
           </Link>
         </div>
-        <div className="sideBarMessagesPage">
+        <div className={styles.sideBarMessagesPage}>
           <Link href="/pages/messagesPage">
-            <Image src={messageButton} width={30} height={25} alt="Ara" onClick={() => handlePageChange("searchButton")} />
+            <Image src={messageButton} width={30} height={25} alt="Mesajlar" />
           </Link>
         </div>
-        <div className="sideBarListsPage">
+        <div className={styles.sideBarListsPage}>
           <Link href="/pages/listsPage">
-            <Image src={ListsButton} width={30} height={25} alt="Ara" onClick={() => handlePageChange("searchButton")} />
+            <Image src={ListsButton} width={30} height={25} alt="Listeler" />
           </Link>
         </div>
-        <div className="sideBarFavoritesPage">
+        <div className={styles.sideBarFavoritesPage}>
           <Link href="/pages/favoritesPage">
-            <Image src={favoritesButton} width={30} height={25} alt="Ara" onClick={() => handlePageChange("searchButton")} />
+            <Image src={favoritesButton} width={30} height={25} alt="Favoriler" />
           </Link>
         </div>
-        <div className="sideBarGroupsPage">
+        <div className={styles.sideBarGroupsPage}>
           <Link href="/pages/groupsPage">
-            <Image src={groupsButton} width={35} height={30} alt="Ara" onClick={() => handlePageChange("searchButton")} />
+            <Image src={groupsButton} width={35} height={30} alt="Gruplar" />
           </Link>
         </div>
-        <div className="sideBarProfilesPage">
+        <div className={styles.sideBarProfilesPage}>
           <Link href="/pages/profilesPage">
-            <Image src={profilesButton} width={30} height={25} alt="Ara" onClick={() => handlePageChange("searchButton")} />
+            <Image src={profilesButton} width={30} height={25} alt="Profiller" />
           </Link>
         </div>
-        <div className="sideBarMorePage">
+        <div className={styles.sideBarMorePage}>
           <Link href="/pages/morePage">
-            <Image src={moreButton} width={30} height={25} alt="Ara" onClick={() => handlePageChange("searchButton")} />
+            <Image src={moreButton} width={30} height={25} alt="Daha Fazla" />
           </Link>
         </div>
-        <div className="sideBarSendPage">
+        <div className={styles.sideBarSendPage}>
           <Link href="/pages/sendPage">
-            <Image className="sideBarSendPageHref" src={sendButton} width={30} height={25} alt="Ara" onClick={() => handlePageChange("searchButton")} />
+            <Image className={styles.sideBarSendPageHref} src={sendButton} width={30} height={25} alt="Gönder" />
           </Link>
         </div>
       </div>
-      <div className="mainShowActivePage">
-        {activePage === "home" && (
-          <div>
-            <MainPage></MainPage>
-          </div>
-        )}
-        {activePage === "search" && (
-          <div>
-            <SearchButtonPage></SearchButtonPage>
-          </div>
-        )}
-        {/* Diğer sayfa içerikleri burada render edilmeli */}
+      <div className={styles.mainShowActivePage}>
+        {activePage === "home" && <MainPage />}
+        {activePage === "search" && <SearchButtonPage />}
       </div>
     </div>
   );
