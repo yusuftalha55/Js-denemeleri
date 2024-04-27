47add4034df84ab8bda63fb66853b0733cd92855
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-04-27 16:44:56 +0300

    Ana sayfada bölümlendirme işlemine başladım

--- twitter-clone/app/pages/homePage/page.tsx
+++ twitter-clone/app/pages/homePage/page.tsx
@@ -1,8 +1,21 @@
-import Home from '@/app/page'
-import React from 'react'
+import Home from "@/app/page";
+import React from "react";
 
 export default function MainPage() {
   return (
-    <div>MainPage</div>
-  )
+    <div>
+      <div className="headerMainPage">
+        <h1>Headers</h1>
+      </div>
+      <div className="sendMainPage">
+        <h1>Send</h1>
+      </div>
+      <div className="newFlowMainPage">
+        <h1>Flow</h1>
+      </div>
+      <div className="containersMainPage">
+        <h1>Containers</h1>
+      </div>
+    </div>
+  );
 }
