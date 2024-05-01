3342f5f8808e522a43dc162d42cf425702d3c45e
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-05-01 15:19:59 +0300

    Sayfanın en iyi ve son hali

--- twitter-clone/app/components/sendSection.tsx
+++ twitter-clone/app/components/sendSection.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Image from "next/image";
+import profilePhotos from "../../app/public/img/profilePhoto.png";
 
 export default function SendSection() {
   return (
     <div className="mainSendSection">
       <div className="mainSendSectionImg">
-        
+        <button className="mainSendSectionImgButton">
+          <Image src={profilePhotos} width={30} height={25} alt="Profil" />
+        </button>
       </div>
     </div>
   );
