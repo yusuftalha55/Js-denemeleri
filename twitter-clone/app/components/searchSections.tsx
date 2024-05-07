f060931b9143f268fb3e879e7914e36be7dce40e
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-05-08 00:37:03 +0300

    sitenin en iyi ve son hali

--- twitter-clone/app/components/searchSections.tsx
+++ twitter-clone/app/components/searchSections.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import Image from 'next/image';
+import searchButton from "../../app/public/img/searchButton.png";
 
 export default function SearchSections() {
   return (
-    <div className='searchSection'>searchSections</div>
+    <div className='searchSection'>
+      <div>
+        <div className='searchSectionSearchButtonImg'>
+          <Image className="searchSectionSearchButtonImg" src={searchButton} width={30} height={25} alt="arama" />
+        </div>
+        <div className='searchSectionButton'>
+          <button className='searchSectionButton'></button>
+        </div>
+      </div>
+    </div>
   )
 }
