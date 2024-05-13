d15e3d3d956eefe3cf3642704010cb98a97bdb3a
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-05-13 14:24:05 +0300

    containers içindeki üst bölümün sekmelerini ekledim

--- twitter-clone/app/components/containers.tsx
+++ twitter-clone/app/components/containers.tsx
@@ -2,6 +2,22 @@ import React from 'react'
 
 export default function Containers() {
   return (
-    <div className='conatinersMain'>containers</div>
+    <div className='conatinersMain'>
+      <div className='conatinersMainLogo'>
+        Logo
+      </div>
+      <div className='conatinersMainName'>
+        Name
+      </div>
+      <div className='conatinersMainTag'>
+        Tag
+      </div>
+      <div className='conatinersMainTime'>
+        Time
+      </div>
+      <div className='conatinersMainMore'>
+        More
+      </div>
+    </div>
   )
 }
