4539f64e7e3571fb8644445f12ba9f54452d56a6
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-05-13 14:32:13 +0300

    conatinersMainBottom bitti

--- twitter-clone/app/components/containers.tsx
+++ twitter-clone/app/components/containers.tsx
@@ -14,6 +14,14 @@ export default function Containers() {
         <div className="conatinersMainText">Text</div>
         <div className="conatinersMainImg">İmg</div>
       </div>
+      <div className="conatinersMainBottom">
+        <div className="answer">Answer</div>
+        <div className="rTweet">Yeniden Gönder</div>
+        <div className="like">Like</div>
+        <div className="views">Views</div>
+        <div className="favorites">Favorites</div>
+        <div className="share">Share</div>
+      </div>
     </div>
   );
 }
