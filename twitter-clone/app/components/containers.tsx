771dd22aa2def92a7acd09ce07a5809ca3b30362
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-05-13 14:25:03 +0300

    div düzenlemesi

--- twitter-clone/app/components/containers.tsx
+++ twitter-clone/app/components/containers.tsx
@@ -1,23 +1,15 @@
-import React from 'react'
+import React from "react";
 
 export default function Containers() {
   return (
-    <div className='conatinersMain'>
-      <div className='conatinersMainLogo'>
-        Logo
-      </div>
-      <div className='conatinersMainName'>
-        Name
-      </div>
-      <div className='conatinersMainTag'>
-        Tag
-      </div>
-      <div className='conatinersMainTime'>
-        Time
-      </div>
-      <div className='conatinersMainMore'>
-        More
+    <div className="conatinersMain">
+      <div className="conatinersMainTop">
+        <div className="conatinersMainLogo">Logo</div>
+        <div className="conatinersMainName">Name</div>
+        <div className="conatinersMainTag">Tag</div>
+        <div className="conatinersMainTime">Time</div>
+        <div className="conatinersMainMore">More</div>
       </div>
     </div>
-  )
+  );
 }
