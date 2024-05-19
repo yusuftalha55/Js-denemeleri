9d0e03e1436f0af0028a3e14ac40061e154d77b5
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-05-19 17:48:28 +0300

    chapters eklendi

---
+++ twitter-clone/app/components/searchButtonPageOptions.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function SearchButtonPageOptions() {
+  return (
+    <div className="searchSectionInTheSearchButtonPageChapters">
+      <div className="searchButtonPageOptionsChapterOne">
+        searchButtonPageOptionsChapterOne
+      </div>
+      <div className="searchButtonPageOptionsChapterTwo">
+        searchButtonPageOptionsChapterOne
+      </div>
+      <div className="searchButtonPageOptionsChapterThree">
+        searchButtonPageOptionsChapterOne
+      </div>
+      <div className="searchButtonPageOptionsChapterFor">
+        searchButtonPageOptionsChapterOne
+      </div>
+      <div className="searchButtonPageOptionsChapterFive">
+        searchButtonPageOptionsChapterOne
+      </div>
+    </div>
+  );
+}
--- twitter-clone/app/pages/searchButtonPage/page.tsx
+++ twitter-clone/app/pages/searchButtonPage/page.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import TrendsContainer from "@/app/components/trendsContainer";
 import SearchButtonSearchSections from "@/app/components/searchButtonPageSearchContainer";
 import ShowMore from "@/app/components/showMore";
+import SearchButtonPageOptions from "@/app/components/searchButtonPageOptions";
 
 export default function SearchButtonPage() {
   return (
     <div>
       <div className="searchSectionInTheSearchButtonPage">
         <SearchButtonSearchSections/>
+      </div>
+      <div className="searchSectionInTheOptions">
+        <SearchButtonPageOptions/>
       </div>
       <div className="trendsContainerSearchButtonPage">
         <TrendsContainer />
