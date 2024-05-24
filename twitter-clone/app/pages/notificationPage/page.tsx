e43be62a2bfba0412186da5b719d0802cf72321c
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-05-24 17:47:12 +0300

    3 chapter eklendi

--- twitter-clone/app/pages/notificationPage/page.tsx
+++ twitter-clone/app/pages/notificationPage/page.tsx
@@ -1,20 +1,32 @@
-import React from 'react'
+import React from "react";
 import settings from "../../../app/public/img/settings.png";
-import Image from 'next/image';
+import Image from "next/image";
 
 export default function NotificationPage() {
   return (
     <div>
-      <div className='NotificationPageTop'>
-        <h2 className='NotificationPageTopHeader'>Bildirimler</h2>
-        <button className='NotificationPageTopButton'><Image src={settings} alt='settings' className='NotificationPageTopImg'/></button>
+      <div className="NotificationPageTop">
+        <h2 className="NotificationPageTopHeader">Bildirimler</h2>
+        <button className="NotificationPageTopButton">
+          <Image
+            src={settings}
+            alt="settings"
+            className="NotificationPageTopImg"
+          />
+        </button>
       </div>
-      <div className='NotificationPageMiddle'>
-        3 chapter
+      <div className="NotificationPageMiddle">
+        <div className="NotificationPageMiddleChapterOne">
+          NotificationPageMiddleChapterOne
+        </div>
+        <div className="NotificationPageMiddleChapterTwo">
+          NotificationPageMiddleChapterTwo
+        </div>
+        <div className="NotificationPageMiddleChapterThree">
+          NotificationPageMiddleChapterThree
+        </div>
       </div>
-      <div className='NotificationPageBottom'>
-        Bildirims
-      </div>
+      <div className="NotificationPageBottom">Bildirims</div>
     </div>
-  )
+  );
 }
