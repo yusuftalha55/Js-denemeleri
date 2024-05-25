f4db5f3cf0dcda7019f33cdb20edc44f7464b8d3
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-05-25 19:25:57 +0300

    views ve sayacı eklendi

--- twitter-clone/app/components/containers.tsx
+++ twitter-clone/app/components/containers.tsx
@@ -1,10 +1,14 @@
+"use client";
+import { useState } from "react";
 import React from "react";
 import Image from "next/image";
 import answer from "../../app/public/img/answer.png";
 import reTweet from "../../app/public/img/arrow.png";
 import like from "../../app/public/img/like.png";
+import views from "../../app/public/img/statistics.png";
 
 export default function Containers() {
+  const [viewsCount, setViewsCount] = useState(15);
   return (
     <div className="conatinersMain">
       <div className="conatinersMainTop">
@@ -22,7 +26,7 @@ export default function Containers() {
         <div className="answer"><Image src={answer} className="answerImg" alt="answer" /></div>
         <div className="rTweet"><Image src={reTweet} className="reTweetImg" alt="reTweet" /></div>
         <div className="like"><Image src={like} className="likeImg" alt="reTweet" /></div>
-        <div className="views">Views</div>
+        <div className="views"><div className="like"><Image src={views} className="likeImg" alt="reTweet" /></div>{viewsCount}</div>
         <div className="favorites">Favorites</div>
         <div className="share">Share</div>
       </div>
