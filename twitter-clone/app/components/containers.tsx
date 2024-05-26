071dfa6943634fb0a0647cb081d185eedd28b78f
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-05-26 15:50:29 +0300

    css ve ikon eklemeleri

--- twitter-clone/app/components/containers.tsx
+++ twitter-clone/app/components/containers.tsx
@@ -6,6 +6,8 @@ import answer from "../../app/public/img/answer.png";
 import reTweet from "../../app/public/img/arrow.png";
 import like from "../../app/public/img/like.png";
 import views from "../../app/public/img/statistics.png";
+import favorites from "../../app/public/img/favorites.png";
+import share from "../../app/public/img/share.png";
 
 export default function Containers() {
   const [viewsCount, setViewsCount] = useState(15);
@@ -27,8 +29,8 @@ export default function Containers() {
         <div className="rTweet"><Image src={reTweet} className="reTweetImg" alt="reTweet" /></div>
         <div className="like"><Image src={like} className="likeImg" alt="like" /></div>
         <div className="views"><Image src={views} className="viewsImg" alt="views" />{viewsCount}</div>
-        <div className="favorites">Favorites</div>
-        <div className="share">Share</div>
+        <div className="favorites"><Image src={favorites} className="favoritesImg" alt="views" /></div>
+        <div className="share"><Image src={share} className="shareImg" alt="views" /></div>
       </div>
     </div>
   );
