eef67d8641eed37975c973c6695e438610c79a3b
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-05-31 16:21:23 +0300

    ufak düzeltme

--- twitter-clone/app/components/context.tsx
+++ twitter-clone/app/components/context.tsx
@@ -1,9 +1,7 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState } from "react";
 
-// Context oluşturma
 export const textContext = createContext({});
 
-// Provider bileşeni
 export const TextProvider = ({ children }) => {
   const [changeTextContext, setChangeTextContext] = useState("Initial Text");
 
