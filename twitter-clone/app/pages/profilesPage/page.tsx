44beb889398a947e643971f1dfed829a8946526a
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-06-02 21:12:38 +0300

    sitenin en iyi ve son hali

--- twitter-clone/app/pages/profilesPage/page.tsx
+++ twitter-clone/app/pages/profilesPage/page.tsx
@@ -1,22 +1,29 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import back from "../../../app/public/img/back.png";
 
 export default function ProfilesPage() {
   return (
-    <div className='profilesPageMain'>
-      <div className='profilesPageTop'>
-        <div className='profilesPageTopBackButton'>
-          <h1>Atkosturanadam</h1>
-
+    <div className="profilesPageMain">
+      <div className="profilesPageTop">
+        <div className="profilesPageTopBackButton">
+          <button>
+            {" "}
+            <Image
+              src={back}
+              alt="settings"
+              className="profilesPageTopBackButtonIn"
+            />
+          </button>
+          <h1 className="profilesPageTopBackButtonText">Atkosturanadam</h1>
         </div>
-        <div className='profilesPageTopProfilesImg'>
-        <h1>asfsafsaf</h1>
+        <div className="profilesPageTopProfilesImg">
+          <h1>asfsafsaf</h1>
         </div>
-        <div className='profilesPageTopMainSection'>
-        <h1>asfsafsaf</h1>
+        <div className="profilesPageTopMainSection">
+          <h1>asfsafsaf</h1>
         </div>
-
-
       </div>
     </div>
-  )
+  );
 }
