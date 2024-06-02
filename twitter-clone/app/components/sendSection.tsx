c023b83439851bb1ac3368c08b3568ae7ba7948a
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-06-02 20:45:00 +0300

    sitenin en iyi ve son hali

--- twitter-clone/app/components/sendSection.tsx
+++ twitter-clone/app/components/sendSection.tsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
-import { useState } from "react";
 import profilePhotos from "../../app/public/img/profilePhoto.png";
 
 export default function SendSection() {
   const [text, setText] = useState('Neler oluyor?');
+  const [isFocused, setIsFocused] = useState(false);
 
+  const handleFocus = () => {
+    if (text === 'Neler oluyor?') {
+      setText('');
+    }
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    if (text === '') {
+      setText('Neler oluyor?');
+    }
+    setIsFocused(false);
+  };
 
   return (
     <div className="mainSendSection">
@@ -22,7 +35,13 @@ export default function SendSection() {
       </div>
       <div className="mainSendSectionInputArea">
         <label>
-          <textarea className="mainSendSectionInputAreaIn" value={text} onChange={(e) => setText(e.target.value)} />
+          <textarea
+            className="mainSendSectionInputAreaIn"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          />
         </label>
       </div>
       <div className="mainSendSectionSendButton">
@@ -31,3 +50,4 @@ export default function SendSection() {
     </div>
   );
 }
+
