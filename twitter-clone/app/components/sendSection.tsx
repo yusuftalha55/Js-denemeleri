f40a4f97fbe4ee175282fea1529019f6979fc8e7
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-06-02 20:43:05 +0300

    gönder kısmına label ekledim

--- twitter-clone/app/components/sendSection.tsx
+++ twitter-clone/app/components/sendSection.tsx
@@ -1,14 +1,29 @@
 import React from "react";
 import Image from "next/image";
+import { useState } from "react";
 import profilePhotos from "../../app/public/img/profilePhoto.png";
 
 export default function SendSection() {
+  const [text, setText] = useState('Neler oluyor?');
+
+
   return (
     <div className="mainSendSection">
       <div className="mainSendSectionImg">
         <button className="mainSendSectionImgButton">
-          <Image className="mainSendSectionImgButtonIn" src={profilePhotos} width={30} height={25} alt="Profil" />
+          <Image
+            className="mainSendSectionImgButtonIn"
+            src={profilePhotos}
+            width={30}
+            height={25}
+            alt="Profil"
+          />
         </button>
+      </div>
+      <div className="mainSendSectionInputArea">
+        <label>
+          <textarea className="mainSendSectionInputAreaIn" value={text} onChange={(e) => setText(e.target.value)} />
+        </label>
       </div>
       <div className="mainSendSectionSendButton">
         <button className="mainSendSectionSendButtonIn">Gönder</button>
