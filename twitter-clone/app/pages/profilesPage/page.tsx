7c7613dbcdb1515de50e20468ce34b516b6dc56c
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-06-03 21:25:00 +0300

    css eklemeleri

--- twitter-clone/app/pages/profilesPage/page.tsx
+++ twitter-clone/app/pages/profilesPage/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import back from "../../../app/public/img/back.png";
 import profilesImage from "../../../app/public/img/galataTower.jpg";
+import profilesPhoto from "../../../app/public/img/profilePhoto.png";
 
 export default function ProfilesPage() {
   return (
@@ -18,14 +19,20 @@ export default function ProfilesPage() {
           <h3 className="profilesPageTopBackButtonText">Atkosturanadam</h3>
         </div>
         <div className="profilesPageTopProfilesImg">
-        <Image
-              src={profilesImage}
-              alt="settings"
-              className="profilesPageTopProfilesImgIn"
-            />
+          <Image
+            src={profilesImage}
+            alt="settings"
+            className="profilesPageTopProfilesImgIn"
+          />
         </div>
         <div className="profilesPageTopMainSection">
-          <h1>asfsafsaf</h1>
+          <div className="profilesPageTopMainSectionProfilesPhoto">
+            <Image
+              src={profilesPhoto}
+              alt="settings"
+              className="profilesPageTopMainSectionProfilesPhotoImg"
+            />
+          </div>
         </div>
       </div>
     </div>
