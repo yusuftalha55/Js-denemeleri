5a9badab3d3e17ffa2ce78272b0a6fe7ce630ab7
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-06-06 14:10:24 +0300

    sitenin en iyi ve son hali

--- twitter-clone/app/pages/profilesPage/page.tsx
+++ twitter-clone/app/pages/profilesPage/page.tsx
@@ -1,12 +1,25 @@
 import React, { useState } from "react";
 import Image from "next/image";
+import Containers from "@/app/components/containers";
 import back from "../../../app/public/img/back.png";
 import profilesImage from "../../../app/public/img/galataTower.jpg";
 import profilesPhoto from "../../../app/public/img/profilePhoto.png";
+import warOne from "../../../app/public/img/war1.jpg";
+import warTwo from "../../../app/public/img/war2.jpg";
+import galata from "../../../app/public/img/galataTower.jpg";
+import sultanAhmet from "../../../app/public/img/sultanahmetCami.jpg";
 
 export default function ProfilesPage() {
-  const [following, setFollowing ] = useState(15);
-  const [followers, setfollowers ] = useState(4);
+  const [following, setFollowing] = useState(15);
+  const [followers, setfollowers] = useState(4);
+  const data = [
+    { image: warOne, text: "Savaş bir felakettir.", nickNameText: "atkosturanadam55" },
+    { image: warTwo, text: "Barış için dua edin.", nickNameText: "atkosturanadam55" },
+    { image: galata, text: "Gökyüzü ve Galata Kulesi.", nickNameText: "atkosturanadam55" },
+    { image: sultanAhmet, text: "Ne güzel ve büyük bir cami.", nickNameText: "atkosturanadam55" },
+  ];
+
+
   return (
     <div className="profilesPageMain">
       <div className="profilesPageTop">
@@ -45,8 +58,12 @@ export default function ProfilesPage() {
           <h2>Atkosturanadam</h2>
           <p>@Atkosturanadam1</p>
           <div className="followersNumbers">
-            <button className="followersNumbersFollowing">{following} Takip edilen</button>
-            <button className="followersNumbersFollowers">{followers} Takipçi</button>
+            <button className="followersNumbersFollowing">
+              {following} Takip edilen
+            </button>
+            <button className="followersNumbersFollowers">
+              {followers} Takipçi
+            </button>
           </div>
         </div>
         <div className="profilesPageTopBottom">
@@ -56,6 +73,18 @@ export default function ProfilesPage() {
           <p className="profilesPageTopBottomFour">Makaleler</p>
           <p className="profilesPageTopBottomFive">Medya</p>
           <p className="profilesPageTopBottomSix">Beğeni</p>
+        </div>
+      </div>
+      <div className="profilePageBottom">
+        <div className="mainContent">
+          {data.map((item, index) => (
+            <Containers
+              key={index}
+              image={item.image}
+              text={item.text}
+              nickNameText={item.nickNameText}
+            />
+          ))}
         </div>
       </div>
     </div>
