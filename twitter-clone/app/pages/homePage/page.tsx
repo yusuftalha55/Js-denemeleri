3f4e4bfc743d4a42634714d0818d58af0c404fdf
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-06-07 14:31:34 +0300

    sitenin en iyi ve son hali

--- twitter-clone/app/pages/homePage/page.tsx
+++ twitter-clone/app/pages/homePage/page.tsx
@@ -11,29 +11,35 @@ import warTwo from "../../../app/public/img/war2.jpg";
 import galata from "../../../app/public/img/galataTower.jpg";
 import sultanAhmet from "../../../app/public/img/sultanahmetCami.jpg";
 
-
 const data = [
-  { image: warOne, text: "Savaş bir felakettir.", nickNameText: "atkosturanadam55" },
-  { image: warTwo, text: "Barış için dua edin.", nickNameText: "atkosturanadam55" },
-  { image: galata, text: "Gökyüzü ve Galata Kulesi.", nickNameText: "atkosturanadam55" },
-  { image: sultanAhmet, text: "Ne güzel ve büyük bir cami.", nickNameText: "atkosturanadam55" },
+  {
+    image: warOne,
+    text: "Savaş bir felakettir.",
+    nickNameText: "atkosturanadam55",
+  },
+  {
+    image: warTwo,
+    text: "Barış için dua edin.",
+    nickNameText: "atkosturanadam55",
+  },
+  {
+    image: galata,
+    text: "Gökyüzü ve Galata Kulesi.",
+    nickNameText: "atkosturanadam55",
+  },
+  {
+    image: sultanAhmet,
+    text: "Ne güzel ve büyük bir cami.",
+    nickNameText: "atkosturanadam55",
+  },
 ];
 
 export default function MainPage() {
   return (
     <div>
       <div className="headerMainPage">
-        <div className="chapterOneMainPage">
-          <button className="chapterOneMainPageButton">Sana Özel</button>
-        </div>
-        <div className="chapterTwoMainPage">
-          <button className="chapterTwoMainPageButton">Takip edilenler</button>
-        </div>
-        <div className="chapterThreeMainPage">
-          <button className="chapterThreeMainPageButton">
-            <Image src={settings} className="chapterThreeMainPageButtonInButtonImg" alt="Ana Sayfa" />
-          </button>
-        </div>
+        <button className="chapterOneMainPageButton">Sana Özel</button>
+        <button className="chapterTwoMainPageButton">Takip edilenler</button>
       </div>
 
       <div className="sendMainPage">
@@ -49,11 +55,15 @@ export default function MainPage() {
       <div className="containersMainPage">
         <div className="containersMainPageIn">
           {data.map((item, index) => (
-            <Containers key={index} image={item.image} text={item.text} nickNameText={item.nickNameText} />
+            <Containers
+              key={index}
+              image={item.image}
+              text={item.text}
+              nickNameText={item.nickNameText}
+            />
           ))}
         </div>
       </div>
     </div>
   );
 }
-
