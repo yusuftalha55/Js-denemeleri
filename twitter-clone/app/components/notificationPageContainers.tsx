4536232021f1fcd01b262ae59b6c4f2d2e96465a
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-06-07 16:22:33 +0300

    Sitenin en iyi ve son hali

--- twitter-clone/app/components/notificationPageContainers.tsx
+++ twitter-clone/app/components/notificationPageContainers.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NotificationPageContainers() {
   return (
     <div className='NotificationPageContainersMain'>
-        notificationPageContainers
+        <button className='NotificationPageContainersMainButton'>notificationPageContainers</button>
     </div>
   )
 }
