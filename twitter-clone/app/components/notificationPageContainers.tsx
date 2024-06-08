e5c28c0e6843fe8e298fd2fb7da7c10f0fe9aa5f
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-06-08 15:28:03 +0300

    notification page e eklemeler

--- twitter-clone/app/components/notificationPageContainers.tsx
+++ twitter-clone/app/components/notificationPageContainers.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import heart from "../../app/public/img/kalp.png";
+import galata from "../../app/public/img/galataTower.jpg";
+import messi from "../../app/public/img/messi.png";
 
 export default function NotificationPageContainers() {
   return (
-    <div className='NotificationPageContainersMain'>
-        <button className='NotificationPageContainersMainButton'>notificationPageContainers</button>
+    <div className="NotificationPageContainersMain">
+      <button className="NotificationPageContainersMainButton">
+        <Image src={heart} alt="beğeni" className="NotificationPageTopImg" />
+        <button><Image src={galata} alt="profilFoto" className="NotificationPageTopImg" /></button>
+      </button>
     </div>
-  )
+  );
 }
