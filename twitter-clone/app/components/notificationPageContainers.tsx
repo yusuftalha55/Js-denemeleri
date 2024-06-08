dd7659a0312a1a3f9c3afd2ccfb04c4dfa825232
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-06-08 15:52:22 +0300

    notoficationpagecontainers temel olarak tamamlandı

--- twitter-clone/app/components/notificationPageContainers.tsx
+++ twitter-clone/app/components/notificationPageContainers.tsx
@@ -1,12 +1,10 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import heart from "../../app/public/img/kalp.png";
 import galata from "../../app/public/img/galataTower.jpg";
 
-
 export default function NotificationPageContainers() {
   const [person, setPerson] = useState("ahmet");
-
 
   return (
     <div className="NotificationPageContainersMain">
@@ -28,6 +26,13 @@ export default function NotificationPageContainers() {
         <div className="personİnformation">
           <h3 className="personİnformationState">{person}</h3>
           <p className="personİnformationText">yanıtını beğendi</p>
+        </div>
+        <div className="NotificationPageContainersMainButtonContent">
+          <p>
+            Güne taze bir fincan kahve ve yeni fikirlerle başlamak harika! ☕️✨
+            Bugünü verimli ve pozitif geçirelim. #SabahMotivasyonu
+            #KahveSeverler
+          </p>
         </div>
       </button>
     </div>
