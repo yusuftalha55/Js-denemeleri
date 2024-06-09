ffc1f2dbd00b0096885b15cdf152b33b8f3539b2
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-06-09 16:05:07 +0300

    MessagesHome sayfasına eklemeler

--- twitter-clone/app/components/messagesHome.tsx
+++ twitter-clone/app/components/messagesHome.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Image from 'next/image';
+import setting from "../../app/public/img/settings.png";
+import message from "../../app/public/img/message.png";
 
 export default function MessagesHome() {
   return (
-    <div>
-      <h3>Mesajlar</h3>
+    <div className='MessagesHomeMain'>
+      <div className='MessagesHomeMainTop'>
+        <h3>Mesajlar</h3>
+        <button>
+        <Image src={setting} className="MessagesHomeMainTopImgSetting" alt="answer" />
+        </button>
+        <button><Image src={message} className="MessagesHomeMainTopImgMessage" alt="answer" /></button>
+      </div>
     </div>
   )
 }
--- twitter-clone/app/pages/messagesPage/page.tsx
+++ twitter-clone/app/pages/messagesPage/page.tsx
@@ -3,6 +3,8 @@ import MessagesHome from '@/app/components/messagesHome'
 
 export default function MessagePage() {
   return (
-    <div>message page</div>
+    <div>
+      <MessagesHome/>
+    </div>
   )
 }
