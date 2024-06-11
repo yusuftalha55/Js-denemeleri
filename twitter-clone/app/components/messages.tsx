f52df5d41d4ff362cb338a2e8bdfecdd8c84394b
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-06-11 13:00:43 +0300

    mesaj konteynırına profil fotosu eklendi

--- twitter-clone/app/components/messages.tsx
+++ twitter-clone/app/components/messages.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import kross from "../../app/public/img/kross.png";
 
 export default function Messages() {
   return (
-    <div className='messagesContainers'>
-        messages
+    <div className="messagesContainers">
+      <div className="messagesContainersTop">
+        <Image src={kross} className="containersMainLogoImg" alt="answer" />
+      </div>
     </div>
-  )
+  );
 }
