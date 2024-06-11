6e57d7f69bb90a9c891c106f743df4d5c24a00ac
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-06-11 13:18:59 +0300

    bölüm ve css eklemeleri

--- twitter-clone/app/components/messages.tsx
+++ twitter-clone/app/components/messages.tsx
@@ -1,14 +1,32 @@
 import React from "react";
 import Image from "next/image";
 import kross from "../../app/public/img/kross.png";
+import dot3 from "../../app/public/img/3point.png";
 
 export default function Messages() {
   return (
     <div className="messagesContainers">
       <div className="messagesContainersTop">
-        <Image src={kross} className="messagesContainersTopImg" alt="profilePhoto" />
+        <Image
+          src={kross}
+          className="messagesContainersTopImg"
+          alt="profilePhoto"
+        />
         <h5 className="messagesContainersTopPerson">Ahmet Yılmaz</h5>
         <p className="messagesContainersTopNick">@ahmet55</p>
+        <button className="messagesContainersTopButton">
+          <Image
+            src={dot3}
+            className="messagesContainersTopButtonImg"
+            alt="3point"
+          />
+        </button>
+      </div>
+      <div className="messagesContainersContent">
+        <p className="messagesContainersContentText">
+          Proje şu an güzel ilerliyor umarım yakın zamanda iş bulmak için işime
+          yarar.
+        </p>
       </div>
     </div>
   );
