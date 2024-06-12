551409faafbc397c4c14d09ee40375f129310957
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-06-12 15:28:26 +0300

    favoritesPage tamamlandı

--- twitter-clone/app/pages/favoritesPage/page.tsx
+++ twitter-clone/app/pages/favoritesPage/page.tsx
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
+import Containers from "@/app/components/containers";
 import threeDot from "../../../app/public/img/3point.png";
+import warOne from "../../../app/public/img/war1.jpg";
+import warTwo from "../../../app/public/img/war2.jpg";
+import galata from "../../../app/public/img/galataTower.jpg";
+import sultanAhmet from "../../../app/public/img/sultanahmetCami.jpg";
 
 export default function FavoritesPage() {
+  const [following, setFollowing] = useState(15);
+  const [followers, setfollowers] = useState(4);
+  const data = [
+    {
+      image: warOne,
+      text: "Savaş bir felakettir.",
+      nickNameText: "atkosturanadam55",
+    },
+    {
+      image: warTwo,
+      text: "Barış için dua edin.",
+      nickNameText: "atkosturanadam55",
+    },
+    {
+      image: galata,
+      text: "Gökyüzü ve Galata Kulesi.",
+      nickNameText: "atkosturanadam55",
+    },
+    {
+      image: sultanAhmet,
+      text: "Ne güzel ve büyük bir cami.",
+      nickNameText: "atkosturanadam55",
+    },
+  ];
   return (
     <div className="favoritesPage">
       <div className="favoritesPageTop">
@@ -16,7 +45,16 @@ export default function FavoritesPage() {
           />
         </button>
       </div>
-      <div className="favoritesPageContent"></div>
+      <div className="favoritesPageContent">
+        {data.map((item, index) => (
+          <Containers
+            key={index}
+            image={item.image}
+            text={item.text}
+            nickNameText={item.nickNameText}
+          />
+        ))}
+      </div>
     </div>
   );
 }
