c2910f6a85f5df45f3a99c2e2ecd934922272538
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-06-15 15:43:07 +0300

    morePage chapterlar ve css eklemeleri

--- twitter-clone/app/pages/morePage/page.tsx
+++ twitter-clone/app/pages/morePage/page.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from "react";
 
 export default function MorePage() {
   return (
-    <div className='morePageMain'>
-      
-
+    <div className="morePageMain">
+      <div className="morePageMainChapterOne">
+        morePageMainChapterOne
+      </div>
+      <div className="morePageMainChapterTwo">
+        morePageMainChapterOne
+      </div>
+      <div className="morePageMainChapterThree">
+        morePageMainChapterOne
+      </div>
+      <div className="morePageMainChapterFour">
+        morePageMainChapterOne
+      </div>
     </div>
-  )
+  );
 }
