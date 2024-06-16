270671448c434fbc93f384c48d22b88c079c8ca8
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-06-16 17:50:44 +0300

    css eklemeleri

--- twitter-clone/app/pages/sendPage/page.tsx
+++ twitter-clone/app/pages/sendPage/page.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import Image from "next/image";
 import profile from "../../../app/public/img/profilePhoto.png";
+import exit from "../../../app/public/img/ximg.png";
 
 export default function SendPage() {
   return (
     <div className="sendPageMain">
       <div className="sendPageMainTop">
-        <button className="sendPageMainTopButton"></button>
+        <button className="sendPageMainTopButton">
+        <Image
+          className="sendPageMainTopButtonImg"
+          src={exit}
+          width={30}
+          height={25}
+          alt="exit"
+        />
+        </button>
       </div>
 
       <div className="sendPageMainInput">
