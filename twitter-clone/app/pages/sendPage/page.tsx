b319cdc83311d973cb30be3a19e5678414b13386
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-06-17 22:06:47 +0300

    sendPage e textarea ekledim

--- twitter-clone/app/pages/sendPage/page.tsx
+++ twitter-clone/app/pages/sendPage/page.tsx
@@ -1,9 +1,46 @@
 import React from "react";
+import { useState } from "react";
 import Image from "next/image";
 import profile from "../../../app/public/img/profilePhoto.png";
 import exit from "../../../app/public/img/ximg.png";
 
 export default function SendPage() {
+  const [text, setText] = useState('Neler oluyor?');
+  const [isFocused, setIsFocused] = useState(false);
+  const [messages, setMessages] = useState([]);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleFocus = () => {
+    if (text === 'Neler oluyor?') {
+      setText('');
+    }
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    if (text === '') {
+      setText('Neler oluyor?');
+    }
+    setIsFocused(false);
+  };
+
+  const handleImageChange = (e) => {
+    if (e.target.files && e.target.files[0]) {
+      setSelectedImage(URL.createObjectURL(e.target.files[0]));
+    }
+  };
+
+  const handleSend = () => {
+    if (text !== 'Neler oluyor?' || selectedImage) {
+      setMessages([...messages, { text, image: selectedImage }]);
+      setText('Neler oluyor?');
+      setSelectedImage(null);
+    }
+  };
+
+
+
+
   return (
     <div className="sendPageMain">
       <div className="sendPageMainTop">
@@ -26,6 +63,13 @@ export default function SendPage() {
           height={25}
           alt="Profil"
         />
+                  <textarea
+            className="mainSendSectionInputAreaIn"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          />
       </div>
 
       <div className="sendPageMainSend">
