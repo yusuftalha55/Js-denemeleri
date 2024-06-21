3118bf307823b601261d0f6112faa4c95d1969de
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-06-21 15:10:16 +0300

    sistemin son hali

--- TwitterBackend/Controllers/OgrenciController.cs
+++ TwitterBackend/Controllers/OgrenciController.cs
@@ -4,9 +4,13 @@ namespace basitwebapi.Controllers
 
 {
     [ApiController]
+    [Route("[controller]")]
 
     public class OgrenciController : ControllerBase 
     {
-        
+        [HttpGet]
+        public string Get(){
+            return "1";
+        }
     }
 }
