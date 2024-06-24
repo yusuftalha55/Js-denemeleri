8e7d3cf8a44e82acb3f3b2512d58d71358aca606
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-06-24 15:54:36 +0300

    yeni datalar ekledim

---
+++ TwitterBackend/Models/ContainerData.cs
@@ -0,0 +1,11 @@
+namespace basitwebapi.Models
+{
+    public class ContainerData
+    {
+        public string textContent { get; set; }
+
+        public string imageContent { get; set; }
+
+        public int like { get; set; }
+    }
+}
