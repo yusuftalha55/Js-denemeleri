2e03393a75e5e80d654bf9f2f3cb989b2f44539c
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-07-09 20:29:50 +0300

    ufak deneme ve ilerlemeler

--- twitterBackend/obj/Release/net6.0/twitterBackend.AssemblyInfo.cs
+++ twitterBackend/obj/Release/net6.0/twitterBackend.AssemblyInfo.cs
@@ -11,8 +11,13 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ProvideApplicationPartFactoryAttribute("Microsoft.AspNetCore.Mvc.ApplicationParts.ConsolidatedAssemblyApplicationPartFact" +
-    "ory, Microsoft.AspNetCore.Mvc.Razor")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("twitterBackend")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyProductAttribute("twitterBackend")]
+[assembly: System.Reflection.AssemblyTitleAttribute("twitterBackend")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
 
