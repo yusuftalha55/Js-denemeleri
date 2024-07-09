a8803f0205b6eb281de5121bdcc7f007a6361018
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-07-09 20:32:59 +0300

    eklemeler

---
+++ twitterBackend/Models/Content.cs
@@ -0,0 +1,12 @@
+﻿namespace twitterBackend.Models
+{
+    public class Content
+    {
+        public int ContentId { get; set; }
+        public int UserId { get; set; }
+        public string ContentText { get; set; }
+
+        public User User { get; set; }
+    }
+}
+
