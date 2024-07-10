3a282303a23e9583eafd948a7540fff8de3095de
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-07-10 14:30:00 +0300

    contentcs sınıfı ekledim

---
+++ twitterBackend/Models/Content.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace twitterBackend.Models
+{
+    public class Content
+    {
+        [Key]
+        public int ContentId { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public string ContentText { get; set; }
+    }
+}
+
