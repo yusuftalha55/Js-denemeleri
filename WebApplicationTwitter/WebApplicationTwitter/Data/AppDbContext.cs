d9d07fc7ac5adef135e6fa14ffdcc138dcda147f
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-07-11 16:30:13 +0300

    Projenin en iyi  ve son hali

---
+++ WebApplicationTwitter/WebApplicationTwitter/Data/AppDbContext.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace WebApplicationTwitter.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public AppDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to postgres with connection string from app settings
+            options.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Content>(entity =>
+            {
+                entity.HasKey(e => e.ContentId);
+                // ContentId, UserId ve ContentText sütunları için herhangi bir kısıtlama eklenmediği için varsayılan olarak NOT NULL olacak.
+            });
+
+            modelBuilder.Entity<Kullanici>(entity =>
+            {
+                entity.HasKey(e => e.Idno);
+                // Idno, Kullaniciadi ve Nickname sütunları için herhangi bir kısıtlama eklenmediği için varsayılan olarak NOT NULL olacak.
+            });
+        }
+
+        public DbSet<Content> Contents { get; set; }
+        public DbSet<Kullanici> Kullanicilar { get; set; }
+    }
+
+    public class Content
+    {
+        public int ContentId { get; set; }
+        public int UserId { get; set; }
+        public string ContentText { get; set; }
+    }
+
+    public class Kullanici
+    {
+        public int Idno { get; set; }
+        public string Kullaniciadi { get; set; }
+        public string Nickname { get; set; }
+    }
+}
+
+
+
---
+++ WebApplicationTwitter/WebApplicationTwitter/Migrations/20240711132652_init.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace WebApplicationTwitter.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contents",
+                columns: table => new
+                {
+                    ContentId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    ContentText = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contents", x => x.ContentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Kullanicilar",
+                columns: table => new
+                {
+                    Idno = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Kullaniciadi = table.Column<string>(type: "text", nullable: false),
+                    Nickname = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kullanicilar", x => x.Idno);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contents");
+
+            migrationBuilder.DropTable(
+                name: "Kullanicilar");
+        }
+    }
+}
