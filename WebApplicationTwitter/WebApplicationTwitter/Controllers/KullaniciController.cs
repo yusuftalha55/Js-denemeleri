7398b6aff4842f4d6f5debd8599971db5a07a270
yusuftalha55 <109973228+yusuftalha55@users.noreply.github.com>
2024-07-14 18:17:43 +0300

    controllers klasörü ve endpoint için class oluşturdum

---
+++ WebApplicationTwitter/WebApplicationTwitter/Controllers/KullaniciController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebApplicationTwitter.Data;
+
+namespace WebApplicationTwitter.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class KullaniciController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public KullaniciController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Kullanici>>> GetKullanicilar()
+        {
+            return await _context.Kullanicilar.ToListAsync();
+        }
+    }
+}
+
